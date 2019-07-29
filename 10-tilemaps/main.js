599a8626f184ee9c541865ec339c3e1fef533cb9
Damjan Pavlica <mudroljub@gmail.com>
2019-07-29 23:25:35 +0200

    ostavljen trag

--- 10-tilemaps/main.js
+++ 10-tilemaps/main.js
@@ -1,5 +1,15 @@
 import level1 from './data/level1.js'
 
+const canvas = document.createElement('canvas')
+canvas.width = 800
+canvas.height = 450
+canvas.style.backgroundColor = 'black'
+
+const ctx = canvas.getContext('2d')
+document.body.appendChild(canvas)
+
+// https://mudroljub.github.io/igrica-partizani/#savo-mitraljezac
+
 for (let y = 0; y < level1.length; y++) {
   for (let x = 0; x < level1[y].length; x++) {
     const tile = level1[y][x]
