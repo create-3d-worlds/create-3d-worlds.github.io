c0353f52427ac8a952ed13e4316e7859248dfd8c
Damjan Pavlica <mudroljub@gmail.com>
2019-07-31 22:55:20 +0200

    refactor to clear game.js file

--- 20-3d-tilemaps/game.js
+++ 20-3d-tilemaps/game.js
@@ -1,18 +1,10 @@
 // import * as THREE from '../node_modules/three/src/Three.js'
 import {scene, controls, renderer, camera} from '../utils/3d-scene.js'
-import {createBox, createFloor} from '../utils/3d-helpers.js'
-import model from '../data/small-map.js'
-
-const textures = ['concrete.jpg', 'crate.gif', 'brick.png']
+import {createFloor, createMap} from '../utils/3d-helpers.js'
+import matrix from '../data/small-map.js'
 
 scene.add(createFloor(500, 500, 'ground.jpg'))
-
-// razlicite materijale za enume
-for (let z = 0; z < model.length; z++)
-  for (let x = 0; x < model.length; x++) {
-    const val = model[z][x]
-    if (val) scene.add(createBox(z, x, textures[val - 1]))
-  }
+scene.add(createMap(matrix))
 
 void function gameLoop() {
   requestAnimationFrame(gameLoop)
--- utils/3d-helpers.js
+++ utils/3d-helpers.js
@@ -1,15 +1,5 @@
 const loader = new THREE.TextureLoader()
-
-export function createBox(z, x, file) {
-  const texture = loader.load(`../assets/textures/${file}`)
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ map: texture })
-  const cube = new THREE.Mesh(geometry, material)
-  cube.position.y = 0.5
-  cube.position.z = z
-  cube.position.x = x
-  return cube
-}
+const textures = ['concrete.jpg', 'crate.gif', 'brick.png']
 
 export function createFloor(width = 100, height = 100, file) {
   const texture = loader.load(`../assets/textures/${file}`)
@@ -21,4 +11,23 @@ export function createFloor(width = 100, height = 100, file) {
   const floor = new THREE.Mesh(geometry, material)
   floor.rotation.x = -Math.PI / 2
   return floor
+}
+
+export function createBox(z, x, file, height = 1) {
+  const texture = loader.load(`../assets/textures/${file}`)
+  const geometry = new THREE.BoxGeometry(1, height, 1)
+  const material = new THREE.MeshBasicMaterial({ map: texture })
+  const cube = new THREE.Mesh(geometry, material)
+  cube.position.y = height / 2
+  cube.position.z = z
+  cube.position.x = x
+  return cube
+}
+
+export function createMap(matrix) {
+  const group = new THREE.Group()
+  matrix.forEach((row, z) => row.forEach((val, x) => {
+    if (val) group.add(createBox(z, x, textures[val - 1], 3 - val))
+  }))
+  return group
 }
