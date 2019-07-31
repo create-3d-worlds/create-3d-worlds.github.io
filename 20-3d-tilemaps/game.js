092be03cde6105b43f03373e952d49c98f06cc95
Damjan Pavlica <damjan.pavlica@origo.is>
2019-07-31 17:33:41 +0200

    render 3d tilemap

--- 20-3d-tilemaps/game.js
+++ 20-3d-tilemaps/game.js
@@ -1,23 +1,23 @@
 import * as THREE from '../node_modules/three/src/Three.js'
 import {scene, controls, renderer, camera, loader} from './scene.js'
-// import model from '../maps/small-map.js'
+import model from '../maps/small-map.js'
 
-// console.table(model)
-
-function createCube() {
+function createCube(y, x) {
   const crateTexture = loader.load('../assets/textures/crate.gif')
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshBasicMaterial({ map: crateTexture })
   const cube = new THREE.Mesh(geometry, material)
   cube.position.y = 0.5
+  cube.position.z = y
+  cube.position.x = x
   return cube
 }
 
 function createFloor() {
-  const moonTexture = loader.load('../assets/textures/moon.jpg')
+  const texture = loader.load('../assets/textures/moon.jpg')
   const floorMaterial = new THREE.MeshBasicMaterial({
     side: THREE.DoubleSide,
-    map: moonTexture
+    map: texture
   })
   const floorGeometry = new THREE.PlaneGeometry(100, 100)
   const floor = new THREE.Mesh(floorGeometry, floorMaterial)
@@ -25,7 +25,10 @@ function createFloor() {
   return floor
 }
 
-scene.add(createCube())
+for (let y = 0; y < model.length; y++)
+  for (let x = 0; x < model.length; x++)
+    if (model[y][x]) scene.add(createCube(y, x))
+
 scene.add(createFloor())
 
 void function gameLoop() {
--- 20-3d-tilemaps/scene.js
+++ 20-3d-tilemaps/scene.js
@@ -3,7 +3,7 @@ import { OrbitControls } from '../node_modules/three/examples/jsm/controls/Orbit
 
 export const scene = new THREE.Scene()
 export const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 5
+camera.position.z = 15
 camera.position.y = 1
 
 export const renderer = new THREE.WebGLRenderer()
@@ -12,5 +12,8 @@ document.body.appendChild(renderer.domElement)
 
 export const controls = new OrbitControls(camera, renderer.domElement)
 controls.maxPolarAngle = Math.PI / 2 - 0.1 // prevent bellow ground
+controls.minDistance = 2
+controls.maxDistance = 20
+controls.zoomSpeed = .3
 
 export const loader = new THREE.TextureLoader()
