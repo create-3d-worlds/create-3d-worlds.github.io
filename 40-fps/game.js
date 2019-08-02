bd478b347bb8ca5737a3f28dc4e0928cc7af3f80
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-02 17:59:43 +0200

    cleanup

--- 40-fps/game.js
+++ 40-fps/game.js
@@ -1,10 +1,11 @@
 import { scene, camera, renderer, clock } from '../utils/3d-scene.js'
 import { createFloor, createPlayerBox } from '../utils/3d-helpers.js'
 import keyboard from '../classes/Keyboard.js'
-const { Vector3 } = THREE
+
+const cameraDistance = [0, 50, 200]
+const cube = createPlayerBox(0, 0, 50)
 
 scene.add(createFloor(1000, 1000))
-const cube = createPlayerBox(0, 0, 50)
 scene.add(cube)
 
 function update() {
@@ -12,27 +13,17 @@ function update() {
   const moveDistance = 200 * delta // 200 pixels per second
   const rotateAngle = Math.PI / 2 * delta // 90 degrees per second
 
-  // move
-  if (keyboard.pressed.KeyA) cube.translateX(-moveDistance)
-  if (keyboard.pressed.KeyD) cube.translateX(moveDistance)
   if (keyboard.pressed.KeyW) cube.translateZ(-moveDistance)
   if (keyboard.pressed.KeyS) cube.translateZ(moveDistance)
-  // rotate
-  if (keyboard.pressed.ArrowLeft) cube.rotateOnAxis(new Vector3(0, 1, 0), rotateAngle)
-  if (keyboard.pressed.ArrowRight) cube.rotateOnAxis(new Vector3(0, 1, 0), -rotateAngle)
-  if (keyboard.pressed.ArrowUp) cube.rotateOnAxis(new Vector3(1, 0, 0), -rotateAngle)
-  if (keyboard.pressed.ArrowDown) cube.rotateOnAxis(new Vector3(1, 0, 0), rotateAngle)
-  // reset
-  if (keyboard.pressed.KeyZ) {
-    cube.rotation.set(0, 0, 0)
-    cube.position.set(0, 25, 0)
-  }
+  if (keyboard.pressed.KeyA) cube.rotateY(rotateAngle)
+  if (keyboard.pressed.KeyD) cube.rotateY(-rotateAngle)
 
-  const relativeCameraOffset = new Vector3(0, 50, 200)
-  const cameraOffset = relativeCameraOffset.applyMatrix4(cube.matrixWorld)
-  camera.position.x = cameraOffset.x
-  camera.position.y = cameraOffset.y
-  camera.position.z = cameraOffset.z
+  // TODO: kad gleda gore da ne moze da radi nista drugo, i da se nakon toga automatski vrati u normalu
+  // if (keyboard.pressed.ArrowUp) cube.rotateX(-rotateAngle)
+  // if (keyboard.pressed.ArrowDown) cube.rotateX(rotateAngle)
+
+  const {x, y, z} = new THREE.Vector3(...cameraDistance).applyMatrix4(cube.matrixWorld)
+  camera.position.set(x, y, z)
   camera.lookAt(cube.position)
 }
 
