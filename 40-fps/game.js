bb7d8536c8d8a58853dcb9256b504db96ebd7c7f
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-02 17:29:37 +0200

    m

--- 40-fps/game.js
+++ 40-fps/game.js
@@ -1,6 +1,7 @@
 /* global THREEx */
 import { scene, camera, renderer, clock } from '../utils/3d-scene.js'
 import { createFloor, createPlayerBox } from '../utils/3d-helpers.js'
+const { Vector3 } = THREE
 
 const keyboard = new THREEx.KeyboardState()
 scene.add(createFloor(1000, 1000))
@@ -11,23 +12,17 @@ function update() {
   const delta = clock.getDelta()
   const moveDistance = 200 * delta // 200 pixels per second
   const rotateAngle = Math.PI / 2 * delta // 90 degrees per second
+
   // move
   if (keyboard.pressed('W')) cube.translateZ(-moveDistance)
   if (keyboard.pressed('S')) cube.translateZ(moveDistance)
-  if (keyboard.pressed('Q')) cube.translateX(-moveDistance)
-  if (keyboard.pressed('E')) cube.translateX(moveDistance)
   // rotate
-  if (keyboard.pressed('A')) cube.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotateAngle)
-  if (keyboard.pressed('D')) cube.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotateAngle)
-  if (keyboard.pressed('R')) cube.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotateAngle)
-  if (keyboard.pressed('F')) cube.rotateOnAxis(new THREE.Vector3(1, 0, 0), -rotateAngle)
-  // reset
-  if (keyboard.pressed('Z')) {
-    cube.position.set(0, 25.1, 0)
-    cube.rotation.set(0, 0, 0)
-  }
+  if (keyboard.pressed('A')) cube.rotateOnAxis(new Vector3(0, 1, 0), rotateAngle)
+  if (keyboard.pressed('D')) cube.rotateOnAxis(new Vector3(0, 1, 0), -rotateAngle)
+  if (keyboard.pressed('R')) cube.rotateOnAxis(new Vector3(1, 0, 0), -rotateAngle)
+  if (keyboard.pressed('F')) cube.rotateOnAxis(new Vector3(1, 0, 0), rotateAngle)
 
-  const relativeCameraOffset = new THREE.Vector3(0, 50, 200)
+  const relativeCameraOffset = new Vector3(0, 50, 200)
   const cameraOffset = relativeCameraOffset.applyMatrix4(cube.matrixWorld)
   camera.position.x = cameraOffset.x
   camera.position.y = cameraOffset.y
