7b7cacb0f94081c403d003dcf24db3626cfe6ae5
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-02 18:08:30 +0200

    clean

--- 40-fps/game.js
+++ 40-fps/game.js
@@ -3,28 +3,24 @@ import { createFloor, createPlayerBox } from '../utils/3d-helpers.js'
 import keyboard from '../classes/Keyboard.js'
 
 const cameraDistance = [0, 50, 200]
-const cube = createPlayerBox(0, 0, 50)
+const player = createPlayerBox(0, 0, 50)
 
 scene.add(createFloor(1000, 1000))
-scene.add(cube)
+scene.add(player)
 
 function update() {
   const delta = clock.getDelta()
-  const moveDistance = 200 * delta // 200 pixels per second
-  const rotateAngle = Math.PI / 2 * delta // 90 degrees per second
+  const distance = 200 * delta // 200 pixels per second
+  const angle = Math.PI / 2 * delta // 90 degrees per second
 
-  if (keyboard.pressed.KeyW) cube.translateZ(-moveDistance)
-  if (keyboard.pressed.KeyS) cube.translateZ(moveDistance)
-  if (keyboard.pressed.KeyA) cube.rotateY(rotateAngle)
-  if (keyboard.pressed.KeyD) cube.rotateY(-rotateAngle)
+  if (keyboard.pressed.KeyW) player.translateZ(-distance)
+  if (keyboard.pressed.KeyS) player.translateZ(distance)
+  if (keyboard.pressed.KeyA) player.rotateY(angle)
+  if (keyboard.pressed.KeyD) player.rotateY(-angle)
 
-  // TODO: kad gleda gore da ne moze da radi nista drugo, i da se nakon toga automatski vrati u normalu
-  // if (keyboard.pressed.ArrowUp) cube.rotateX(-rotateAngle)
-  // if (keyboard.pressed.ArrowDown) cube.rotateX(rotateAngle)
-
-  const {x, y, z} = new THREE.Vector3(...cameraDistance).applyMatrix4(cube.matrixWorld)
+  const {x, y, z} = new THREE.Vector3(...cameraDistance).applyMatrix4(player.matrixWorld)
   camera.position.set(x, y, z)
-  camera.lookAt(cube.position)
+  camera.lookAt(player.position)
 }
 
 /* INIT */
