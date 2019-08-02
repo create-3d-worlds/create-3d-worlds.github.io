09cbfdd312790fe3043000dff77b43cc4a09aed4
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-02 18:26:55 +0200

    m

--- 40-fps/game.js
+++ 40-fps/game.js
@@ -2,13 +2,12 @@ import { scene, camera, renderer, clock } from '../utils/3d-scene.js'
 import { createFloor } from '../utils/3d-helpers.js'
 import Player3D from '../classes/Player3D.js'
 
-const cameraDistance = [0, 50, 200]
 const player = new Player3D(0, 0, 50)
-
 scene.add(createFloor(1000, 1000))
 scene.add(player.mesh)
 
 function updateCamera() {
+  const cameraDistance = [0, 50, 200]
   const {x, y, z} = new THREE.Vector3(...cameraDistance).applyMatrix4(player.mesh.matrixWorld)
   camera.position.set(x, y, z)
   camera.lookAt(player.mesh.position)
@@ -17,8 +16,8 @@ function updateCamera() {
 /* INIT */
 
 void function animate() {
-  const delta = clock.getDelta()
   requestAnimationFrame(animate)
+  const delta = clock.getDelta()
   updateCamera()
   player.update(delta)
   renderer.render(scene, camera)
