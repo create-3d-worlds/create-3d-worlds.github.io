6615d56b7868faec446d0566741f0428b36353bd
Damjan Pavlica <mudroljub@gmail.com>
2019-08-03 11:40:56 +0200

    delete raycaster

--- 45-pointer-lock-controls/game.js
+++ 45-pointer-lock-controls/game.js
@@ -1,11 +1,7 @@
 import { PointerLockControls } from '../node_modules/three/examples/jsm/controls/PointerLockControls.js'
-import { scene, camera, renderer } from '../utils/3d-scene.js'
+import { scene, camera, renderer, clock } from '../utils/3d-scene.js'
 import {createTerrain, createRandomCubes} from '../utils/3d-helpers.js'
 import keyboard from '../classes/Keyboard.js'
-
-let canJump = false
-let prevTime = performance.now()
-const velocity = new THREE.Vector3()
 
 const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75)
 light.position.set(0.5, 1, 0.75)
@@ -18,15 +14,12 @@ scene.add(cubes)
 
 scene.add(createTerrain())
 
-const raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10)
+const velocity = new THREE.Vector3()
+let canJump = false
 
 function updateControls() {
-  raycaster.ray.origin.copy(controls.getObject().position)
-  raycaster.ray.origin.y -= 10
-  const intersections = raycaster.intersectObjects(cubes.children)
-  const isOnObject = intersections.length > 0
-  const time = performance.now()
-  const delta = (time - prevTime) / 1000
+  const mesh = controls.getObject()
+  const delta = clock.getDelta()
   velocity.x -= velocity.x * 10.0 * delta
   velocity.z -= velocity.z * 10.0 * delta
   velocity.y -= 9.8 * 100.0 * delta // 100.0 = mass
@@ -38,19 +31,14 @@ function updateControls() {
     if (canJump === true) velocity.y += 350
     canJump = false
   }
-  if (isOnObject === true) {
-    velocity.y = Math.max(0, velocity.y)
-    canJump = true
-  }
-  controls.getObject().translateX(velocity.x * delta)
-  controls.getObject().translateY(velocity.y * delta)
-  controls.getObject().translateZ(velocity.z * delta)
-  if (controls.getObject().position.y < 10) {
+  mesh.translateX(velocity.x * delta)
+  mesh.translateY(velocity.y * delta)
+  mesh.translateZ(velocity.z * delta)
+  if (mesh.position.y < 10) {
     velocity.y = 0
-    controls.getObject().position.y = 10
+    mesh.position.y = 10
     canJump = true
   }
-  prevTime = time
 }
 
 void function animate() {
