4e0b6f4ca6d98eaddaddbe7a24c004a70ea3041d
Damjan Pavlica <mudroljub@gmail.com>
2019-08-03 10:13:34 +0200

    separate helpers

--- 45-threejs-fps-controls/game.js
+++ 45-threejs-fps-controls/game.js
@@ -1,10 +1,8 @@
 import { PointerLockControls } from '../node_modules/three/examples/jsm/controls/PointerLockControls.js'
 import { askPointerLock } from './askPointerLock.js'
 import { scene, camera, renderer } from '../utils/3d-scene.js'
-
-let geometry, material, mesh
-const objects = []
-let raycaster
+import {createTerrain, createRandomCubes} from '../utils/3d-helpers.js'
+import keyboard from '../classes/Keyboard.js'
 
 let moveForward = false
 let moveBackward = false
@@ -12,6 +10,7 @@ let moveLeft = false
 let moveRight = false
 let canJump = false
 let prevTime = performance.now()
+
 const velocity = new THREE.Vector3()
 
 const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75)
@@ -20,58 +19,21 @@ scene.add(light)
 
 const controls = new PointerLockControls(camera)
 askPointerLock(controls)
-
 scene.add(controls.getObject())
 
-raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10)
-// floor
-geometry = new THREE.PlaneGeometry(2000, 2000, 100, 100)
-geometry.rotateX(- Math.PI / 2)
+const cubes = createRandomCubes()
+scene.add(cubes)
 
-for (let i = 0, l = geometry.vertices.length; i < l; i++) {
-  const vertex = geometry.vertices[i]
-  vertex.x += Math.random() * 20 - 10
-  vertex.y += Math.random() * 2
-  vertex.z += Math.random() * 20 - 10
-}
+const raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10)
 
-for (let i = 0, l = geometry.faces.length; i < l; i++) {
-  let face = geometry.faces[i]
-  face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-}
-
-material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors })
-mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-// objects
-geometry = new THREE.BoxGeometry(20, 20, 20)
-
-for (let i = 0, l = geometry.faces.length; i < l; i++) {
-  const face = geometry.faces[i]
-  face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-}
-
-for (let i = 0; i < 500; i++) {
-  material = new THREE.MeshPhongMaterial({ specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors })
-  mesh = new THREE.Mesh(geometry, material)
-  mesh.position.x = Math.floor(Math.random() * 20 - 10) * 20
-  mesh.position.y = Math.floor(Math.random() * 20) * 20 + 10
-  mesh.position.z = Math.floor(Math.random() * 20 - 10) * 20
-  scene.add(mesh)
-  material.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  objects.push(mesh)
-}
+scene.add(createTerrain())
 
 void function animate() {
   requestAnimationFrame(animate)
   if (controls.enabled) {
     raycaster.ray.origin.copy(controls.getObject().position)
     raycaster.ray.origin.y -= 10
-    const intersections = raycaster.intersectObjects(objects)
+    const intersections = raycaster.intersectObjects(cubes.children)
     const isOnObject = intersections.length > 0
     const time = performance.now()
     const delta = (time - prevTime) / 1000
--- utils/3d-helpers.js
+++ utils/3d-helpers.js
@@ -74,3 +74,56 @@ export function createTrees(num = 20) {
   coords.map(coord => group.add(createTree(...coord)))
   return group
 }
+
+export function createTerrain() {    
+  const geometry = new THREE.PlaneGeometry(2000, 2000, 100, 100)
+  geometry.rotateX(- Math.PI / 2)
+
+  for (let i = 0, l = geometry.vertices.length; i < l; i++) {
+    const vertex = geometry.vertices[i]
+    vertex.x += Math.random() * 20 - 10
+    vertex.y += Math.random() * 2
+    vertex.z += Math.random() * 20 - 10
+  }
+
+  for (let i = 0, l = geometry.faces.length; i < l; i++) {
+    const face = geometry.faces[i]
+    face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+  }
+
+  const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors })
+  return new THREE.Mesh(geometry, material)
+}
+
+export function createRandomCubes() {
+  const group = new THREE.Group()
+  const geometry = new THREE.BoxGeometry(20, 20, 20)
+  for (let i = 0, l = geometry.faces.length; i < l; i++) {
+    const face = geometry.faces[i]
+    face.vertexColors[0] = new THREE.Color().setHSL(
+      Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75
+    )
+    face.vertexColors[1] = new THREE.Color().setHSL(
+      Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75
+    )
+    face.vertexColors[2] = new THREE.Color().setHSL(
+      Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75
+    )
+  }
+  for (let i = 0; i < 500; i++) {
+    const material = new THREE.MeshPhongMaterial({
+      specular: 0xffffff,
+      shading: THREE.FlatShading,
+      vertexColors: THREE.VertexColors
+    })
+    material.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = Math.floor(Math.random() * 20 - 10) * 20
+    mesh.position.y = Math.floor(Math.random() * 20) * 20 + 10
+    mesh.position.z = Math.floor(Math.random() * 20 - 10) * 20
+    group.add(mesh)
+  }
+  return group
+}
