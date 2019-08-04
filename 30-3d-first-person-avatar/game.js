2f25c0865eefa1b782ba315dadb480e0af2f0f4e
Damjan Pavlica <mudroljub@gmail.com>
2019-08-04 21:35:31 +0200

    PointerLockControls

--- 30-3d-first-person-avatar/game.js
+++ 30-3d-first-person-avatar/game.js
@@ -1,14 +1,17 @@
+import { PointerLockControls } from '../node_modules/three/examples/jsm/controls/PointerLockControls.js'
 import {createTrees, createFloor} from '../utils/3d-helpers.js'
 import {scene, renderer, camera, clock} from '../utils/3d-scene.js'
 import Avatar from '../classes/Avatar.js'
+
+new PointerLockControls(camera)
 
 const avatar = new Avatar()
 scene.add(avatar.mesh)
 scene.add(createTrees())
 scene.add(createFloor(500, 500, 'ground.jpg'))
 
-camera.position.z = 400 // distance from player back
-avatar.mesh.add(camera) // camera is added to player
+camera.position.z = 400 // distance from player
+avatar.mesh.add(camera)
 
 void function animate() {
   requestAnimationFrame(animate)
@@ -16,3 +19,11 @@ void function animate() {
   avatar.update(delta)
   renderer.render(scene, camera)
 }()
+
+/* EVENTS */
+
+document.body.addEventListener('click', document.body.requestPointerLock)
+
+document.addEventListener('pointerlockchange', () => {
+  if (!document.pointerLockElement) camera.lookAt(avatar.mesh.position)
+})
