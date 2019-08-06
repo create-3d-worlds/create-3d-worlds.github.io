9c980ee6af6810c7dbc5bcc7cf6ded9275e8b582
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-06 15:56:36 +0200

    cleanup

--- 60-collision-raycast-avatar/game.js
+++ 60-collision-raycast-avatar/game.js
@@ -1,35 +1,23 @@
-/* global TWEEN */
 import Avatar from '../classes/Avatar.js'
 import {scene, renderer, camera, clock, createOrbitControls} from '../utils/3d-scene.js'
-import {randomInRange} from '../utils/helpers.js'
-import {createTree} from '../utils/3d-helpers.js'
-
-const solids = []
+import {createTrees} from '../utils/3d-helpers.js'
 
 const avatar = new Avatar(0, 0, 1)
 scene.add(avatar.mesh)
 
 camera.position.z = 500
 avatar.add(camera)
-
 createOrbitControls()
 
-/* INIT */
+scene.add(createTrees(10, -1000, 1000, 200))
 
-// TODO: createTrees
-const coords = Array(5).fill().map(() => [randomInRange(-1000, 1000), randomInRange(-1000, 1000)])
-coords.map(coord => {
-  const tree = createTree(...coord, 200)
-  scene.add(tree)
-})
-
+const solids = []
 scene.traverse(child => {
   if (child.name == 'solid') solids.push(child)
 })
 
 void function animate() {
   requestAnimationFrame(animate)
-  TWEEN.update()
   const delta = clock.getDelta()
   avatar.update(delta, solids)
   renderer.render(scene, camera)
