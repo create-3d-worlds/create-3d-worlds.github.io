a2bd9b8564cd75d435fd772af1aefff4294b4e4f
Damjan Pavlica <mudroljub@gmail.com>
2019-08-09 14:28:02 +0200

    cleanup

--- 80-procedural-city/game.js
+++ 80-procedural-city/game.js
@@ -18,29 +18,15 @@ scene.add(createPlane(2000, 2000, 0x101018))
 /* FUNCTIONS */
 
 function generateBuilding() {
-  const lightColor = new THREE.Color(0xffffff)
-  const shadow = new THREE.Color(0x303050)
   const box = new THREE.CubeGeometry(1, 1, 1)
   box.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0))
 
   const building = new THREE.Mesh(box)
-
-  const value = 1 - Math.random() * Math.random()
-  const color = new THREE.Color().setRGB(value + Math.random() * 0.1, value, value + Math.random() * 0.1)
-
-  const top = color.clone().multiply(lightColor)
-  const bottom = color.clone().multiply(shadow)
-
   building.position.x = randomInRange(-1000, 1000)
   building.position.z = randomInRange(-1000, 1000)
   building.rotation.y = Math.random()
   building.scale.x = building.scale.z = randomInRange(10, 20, false)
   building.scale.y = (Math.random() * Math.random() * Math.random() * building.scale.x) * 8 + 8
-
-  const {geometry} = building
-  for (let j = 0; j < geometry.faces.length; j++)
-    geometry.faces[j].vertexColors = j === 2 ? [color, color, color, color] : [top, bottom, bottom, top]
-
   return building
 }
 
@@ -81,7 +67,7 @@ function createCity(num = 10000) {
   const cityGeo = generateCityGeometry(num)
   const texture = new THREE.Texture(generateCityTexture())
   texture.needsUpdate = true
-  const cityMesh = new THREE.Mesh(cityGeo, new THREE.MeshLambertMaterial({map: texture}))
+  const cityMesh = new THREE.Mesh(cityGeo, new THREE.MeshLambertMaterial()) // {map: texture}
   return cityMesh
 }
 
