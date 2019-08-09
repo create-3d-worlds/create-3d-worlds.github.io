b9ffdff10640cf1c4add9fe7da5c97c02805b1fc
Damjan Pavlica <mudroljub@gmail.com>
2019-08-10 01:12:55 +0200

    gradjevina

--- 76-procedural-castle/game.js
+++ 76-procedural-castle/game.js
@@ -1,38 +1,37 @@
 import { scene, camera, renderer, createOrbitControls } from '../utils/three-scene.js'
 
-const blockSize = 1
-const blockWidth = blockSize * 2
-const space = .03
+const rowSize = 1
+const spacing = .03
+const brickSize = rowSize + spacing
 const blocksInRow = 20
-const rowsInWall = 10
-const fullBlockWidth = blockWidth + space
-const fullBlockHeight = blockSize + space
-const wallWidth = fullBlockWidth * blocksInRow
+const rows = 10
+const wallWidth = brickSize * blocksInRow
 
 camera.position.set(5, 15, 25)
 createOrbitControls()
 
 /* FUNCTIONS */
 
-function createBlock(x, y, z, rotated) {
-  const block = new THREE.Mesh(new THREE.BoxGeometry(blockWidth, blockSize, blockSize), new THREE.MeshNormalMaterial())
+const isOdd = x => x % 2
+
+function createBlock(x, y, z) {
+  const block = new THREE.Mesh(new THREE.BoxGeometry(rowSize, rowSize, rowSize), new THREE.MeshNormalMaterial())
   block.position.set(x, y, z)
-  if (rotated) block.rotateY(Math.PI / 2)
   return block
 }
 
 function createRow(row, startX, z) {
   const group = new THREE.Group()
-  for (let x = 0; x < wallWidth; x += fullBlockWidth) {
-    const adjX = row % 2 ? x : x + blockSize / 2
-    group.add(createBlock(startX + adjX, row * fullBlockHeight, z))
+  for (let x = 0; x < wallWidth; x += brickSize) {
+    const adjX = isOdd(row) ? x : x + brickSize / 2
+    group.add(createBlock(startX + adjX, row * brickSize, z))
   }
   return group
 }
 
 function createWall(x, z) {
   const group = new THREE.Group()
-  for (let row = 0; row < rowsInWall; row++) {
+  for (let row = 0; row < rows; row++) {
     group.add(createRow(row, x, z))
   }
   return group
@@ -40,14 +39,18 @@ function createWall(x, z) {
 
 /* INIT **/
 
-var axesHelper = new THREE.AxesHelper(10);
-scene.add( axesHelper );
+var axesHelper = new THREE.AxesHelper(10)
+scene.add(axesHelper)
 
 scene.add(createWall(0, -wallWidth / 2))
 scene.add(createWall(0, wallWidth / 2))
 
-for (let z = -wallWidth / 2; z < wallWidth / 2; z+=fullBlockWidth) {
-  scene.add(createBlock(0, 0, z, true))  
+for (let z = -wallWidth / 2; z < wallWidth / 2; z+=brickSize) {
+  scene.add(createBlock(0, 0, z))  
+}
+
+for (let z = -wallWidth / 2; z < wallWidth / 2; z+=brickSize) {
+  scene.add(createBlock(0, brickSize, z + brickSize / 2))  
 }
 
 void function update() {
