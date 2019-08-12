8679f0f0e3c23112e670571b3eca220a394fbd1e
Damjan Pavlica <damjan.pavlica@origo.is>
2019-08-12 17:15:39 +0200

    cleanup

--- 82-terrain-rpg/game.js
+++ 82-terrain-rpg/game.js
@@ -1,142 +1,141 @@
 /* global SimplexNoise */
 import {scene, renderer, camera, createOrbitControls} from '../utils/three-scene.js'
-// import {createTrees} from '../utils/three-helpers.js'
+import {createTrees} from '../utils/three-helpers.js'
+import {rndInt} from '../utils/helpers.js'
+
+const size = 1000
 
 createOrbitControls()
 camera.position.y = 250
 camera.position.z = 250
 scene.add(new THREE.HemisphereLight())
 
-const generateTerrain = function() {
+const generateTerrain = function(size = 1000, avgHeight = 30) {
   const resolution = 20
-  const material = new THREE.MeshLambertMaterial({ color: 0x33aa33, shading: THREE.FlatShading, vertexColors: THREE.FaceColors, overdraw: true})
-  const geometry = new THREE.PlaneGeometry(1200, 1200, resolution, resolution)
-  geometry.dynamic = true
-  geometry.verticesNeedUpdate = true
-  // geometry.computeCentroids()
+  const landMaterial = new THREE.MeshLambertMaterial({
+    color: 0x33aa33,
+    shading: THREE.FlatShading,
+    vertexColors: THREE.FaceColors,
+    overdraw: true
+  })
+  const landGeo = new THREE.PlaneGeometry(size, size, resolution, resolution)
 
   const noise = new SimplexNoise()
-  let n
-
   const factorX = 50
   const factorY = 25
   const factorZ = 60
 
-  for (let i = 0; i < geometry.vertices.length; i++) {
-    n = noise.noise(geometry.vertices[i].x / resolution / factorX, geometry.vertices[i].y / resolution / factorY)
+  landGeo.vertices.forEach(vertex => {
+    let n = noise.noise(vertex.x / resolution / factorX, vertex.y / resolution / factorY)
     n -= 0.25
-    geometry.vertices[i].z = n * factorZ
-  }
-
-  for (let f = 0; f < geometry.faces.length; f++) {
-    const {color} = geometry.faces[f]
+    vertex.z = n * factorZ
+  })
+  landGeo.faces.forEach(face => {
+    const { color } = face
     const rand = Math.random() / 5
-    geometry.faces[f].color.setRGB(color.r + rand, color.g + rand, color.b + rand)
-  }
+    face.color.setRGB(color.r + rand, color.g + rand, color.b + rand)
+  })
 
-  const land = new THREE.Mesh(geometry, material)
+  const land = new THREE.Mesh(landGeo, landMaterial)
   land.receiveShadow = true
   land.name = 'land'
   land.rotateX(-Math.PI / 2)
-  land.position.set(0, 30, 0)
+  land.position.set(0, avgHeight, 0)
 
-  const water_material = new THREE.MeshLambertMaterial({color: 0x6699ff, transparent: true, opacity: 0.75, vertexColors: THREE.FaceColors, shading: THREE.FlatShading})
-  const water_geometry = new THREE.PlaneGeometry(1200, 1200, resolution, resolution)
-  water_geometry.dynamic = true
-  water_geometry.verticesNeedUpdate = true
-  for (let i = 0; i < water_geometry.faces.length; i++) {
-    const {color} = water_geometry.faces[i]
-    const rand = Math.random()
-    water_geometry.faces[i].color.setRGB(color.r + rand, color.g + rand, color.b + rand)
-  }
+  const waterMaterial = new THREE.MeshLambertMaterial({
+    color: 0x6699ff,
+    transparent: true,
+    opacity: 0.75,
+    vertexColors: THREE.FaceColors,
+    shading: THREE.FlatShading
+  })
+  const waterGeo = new THREE.PlaneGeometry(size, size, resolution, resolution)
+  waterGeo.faces.forEach(face => {
+    const { color } = face
+    const rand = Math.random() / 2
+    face.color.setRGB(color.r + rand, color.g + rand, color.b + rand)
+  })
 
-  const water = new THREE.Mesh(water_geometry, water_material)
+  const water = new THREE.Mesh(waterGeo, waterMaterial)
   water.receiveShadow = true
   water.name = 'water'
   water.rotateX(-Math.PI / 2)
 
   const terrain = new THREE.Object3D()
   terrain.name = 'terrain'
-
   terrain.add(land)
   terrain.add(water)
-
   terrain.receiveShadow = true
-
   return terrain
 }
 
 scene.add(generateTerrain())
 // scene.add(createTrees())
 
-const treeModel = (function() {
-  const treeData = {
-    geom: {
-      leaves: new THREE.CylinderGeometry(0, 25, 60, 4, 1),
-      trunk: new THREE.BoxGeometry(5, 20, 5)
-    },
-    materials: {
-      leaves: new THREE.MeshLambertMaterial({ color: 0x3EA055, shading: THREE.SmoothShading }),
-      trunk: new THREE.MeshLambertMaterial({ color: 0x966F33, shading: THREE.SmoothShading })
-    }
+const createFir = () => {
+  const geom = {
+    leaves: new THREE.CylinderGeometry(0, 25, 60, 4, 1),
+    trunk: new THREE.BoxGeometry(5, 20, 5)
+  }
+  const materials = {
+    leaves: new THREE.MeshLambertMaterial({ color: 0x3EA055, shading: THREE.SmoothShading }),
+    trunk: new THREE.MeshLambertMaterial({ color: 0x966F33, shading: THREE.SmoothShading })
   }
   const tree = new THREE.Object3D()
-  const leaves = new THREE.Mesh(treeData.geom.leaves, treeData.materials.leaves)
-  const trunk = new THREE.Mesh(treeData.geom.trunk, treeData.materials.trunk)
+  const leaves = new THREE.Mesh(geom.leaves, materials.leaves)
+  const trunk = new THREE.Mesh(geom.trunk, materials.trunk)
   leaves.name = 'leaves'
   trunk.name = 'trunk'
-
   leaves.castShadow = true
   trunk.castShadow = true
-
   leaves.position.y += 50
   trunk.position.y += 20
-
   tree.add(leaves)
   tree.add(trunk)
   tree.castShadow = true
-
   return tree
-})()
-
-class Entity {
-  constructor(color = 0xffffff) {
-    this.pos      = new THREE.Vector3(0, 0, 0)
-    this.destination = new THREE.Vector3(0, 0, 0)
-    this.vel      = new THREE.Vector3(0, 0, 0)
-    this.rotation = new THREE.Euler(0, 0, 0)
-    this.timeMult = 1
-    this.remove   = false
-    this.shadow   = false
-    this.state = null
-    this.color = color
-    this.create()
-  }
-
-  create() {
-    const geometry = new THREE.BoxGeometry(10, 10, 10)
-    const material = new THREE.MeshLambertMaterial({ color: 0xff0000, shading: THREE.SmoothShading })
-    this.mesh = new THREE.Mesh(geometry, material)
-    this.mesh.castShadow = true
-  }
 }
 
-class Tree extends Entity {
+class Fir {
   constructor(pos) {
-    super()
     this.name = 'tree'
-    this.pos = pos
-    this.units = 4
-  }
-
-  create() {
-    this.mesh = treeModel.clone()
+    this.mesh = createFir()
+    const {x, y, z} = pos
+    this.mesh.position.set(x, y, z)
   }
 }
 
-const tree = new Tree({x: 0, y: 0, z: 0})
-console.log(tree)
-scene.add(tree.mesh)
+const place = function(position) {
+  const caster = new THREE.Raycaster()
+  const ray = new THREE.Vector3(0, -1, 0)
+  caster.set(position, ray)
+  const land = scene.getObjectByName('terrain').children[0]
+  const collisions = caster.intersectObject(land)
+  if (collisions.length > 0) {
+    console.log(collisions[0].point)
+    return collisions[0].point
+  }
+  return null
+}
+
+const createFirs = function(numTrees = 50) {
+  const group = new THREE.Group()
+  for (let i = 0; i < numTrees; i++) {
+    const rndPoint = new THREE.Vector3(rndInt(size / 2), 100, rndInt(size / 2))
+    const position = place(rndPoint)
+    if (position) {
+      console.log('ima')
+      // if (position.y > 0) {
+      //   position.y -= 10
+      // }
+      group.add(new Fir(position).mesh)
+    }
+  }
+  return group
+}
+
+// scene.add(createFirs())
+scene.add(createTrees())
 
 void function animate() {
   requestAnimationFrame(animate)
