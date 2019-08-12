a1d0263b30992ad0e5b6b86581030a0aa219fe12
Damjan Pavlica <mudroljub@gmail.com>
2019-08-13 00:35:00 +0200

    clean

--- 82-terrain-rpg/game.js
+++ 82-terrain-rpg/game.js
@@ -8,51 +8,58 @@ createOrbitControls()
 camera.position.y = 250
 camera.position.z = 250
 
-const generateTerrain = function(size = 1000, avgHeight = 30) {
+const createHillTerrain = (size, avgHeight = 30) => {
   const resolution = 20
-  const landMaterial = new THREE.MeshLambertMaterial({
+  const material = new THREE.MeshLambertMaterial({
     color: 0x33aa33,
     vertexColors: THREE.FaceColors
   })
-  const landGeo = new THREE.PlaneGeometry(size, size, resolution, resolution)
+  const geometry = new THREE.PlaneGeometry(size, size, resolution, resolution)
 
   const noise = new SimplexNoise()
   const factorX = 50
   const factorY = 25
   const factorZ = 60
-
-  landGeo.vertices.forEach(vertex => {
+  geometry.vertices.forEach(vertex => {
     let n = noise.noise(vertex.x / resolution / factorX, vertex.y / resolution / factorY)
     n -= 0.25
     vertex.z = n * factorZ
   })
-  landGeo.faces.forEach(face => {
+  geometry.faces.forEach(face => {
     const { color } = face
     const rand = Math.random() / 5
     face.color.setRGB(color.r + rand, color.g + rand, color.b + rand)
   })
 
-  const land = new THREE.Mesh(landGeo, landMaterial)
-  land.name = 'land'
-  land.rotateX(-Math.PI / 2)
-  land.position.set(0, avgHeight, 0)
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.name = 'mesh'
+  mesh.rotateX(-Math.PI / 2)
+  // mesh.position.set(0, avgHeight, 0)
+  return mesh
+}
 
-  const waterMaterial = new THREE.MeshLambertMaterial({
+const createWater = size => {
+  const resolution = 10
+  const material = new THREE.MeshLambertMaterial({
     color: 0x6699ff,
     transparent: true,
     opacity: 0.5,
     vertexColors: THREE.FaceColors
   })
-  const waterGeo = new THREE.PlaneGeometry(size, size, resolution, resolution)
-  waterGeo.faces.forEach(face => {
+  const geometry = new THREE.PlaneGeometry(size, size, resolution, resolution)
+  geometry.faces.forEach(face => {
     const { color } = face
     const rand = Math.random() / 2
     face.color.setRGB(color.r + rand, color.g + rand, color.b + rand)
   })
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.rotateX(-Math.PI / 2)
+  return mesh
+}
 
-  const water = new THREE.Mesh(waterGeo, waterMaterial)
-  water.rotateX(-Math.PI / 2)
-
+const generateTerrain = function(size = 1000, avgHeight = 30) {
+  const land = createHillTerrain(size, avgHeight)
+  const water = createWater(size)
   const terrain = new THREE.Object3D()
   terrain.name = 'terrain'
   terrain.add(land)
