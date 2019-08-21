96021b0255992dd2ca6f78483e080d45874a3089
Damjan Pavlica <mudroljub@gmail.com>
2019-08-21 18:47:30 +0200

    wolf dae

--- 68-model-dae/main.js
+++ 68-model-dae/main.js
@@ -1,21 +1,33 @@
 import * as THREE from '../node_modules/three/build/three.module.js'
 import { scene, renderer, camera, clock, createOrbitControls} from '../utils/scene.js'
-import {ColladaLoader} from '../node_modules/three/examples/jsm/loaders/ColladaLoader.js'
+import { ColladaLoader } from '../node_modules/three/examples/jsm/loaders/ColladaLoader.js'
 
-createOrbitControls()
-let mixer
+const controls = createOrbitControls()
+let mixer, a = 0
 
+camera.position.set(0, 0, 1)
+scene.background = new THREE.Color(0xffffff)
+
+// monster.dae
 const loader = new ColladaLoader()
-loader.load('../assets/models/monster.dae', collada => {
+loader.load('../assets/models/wolf-dae/Wolf_With_Baked_Action_Animations_For_Export.dae', collada => {
   const {animations, scene: model} = collada
   mixer = new THREE.AnimationMixer(model)
   mixer.clipAction(animations[0]).play()
+  controls.target = model.position
   scene.add(model)
+
+  document.addEventListener('click', () => {
+    const animation = animations[++a % animations.length]
+    console.log(animation)
+    mixer.clipAction(animation).play()
+  })
 })
 
 void function render() {
   const delta = clock.getDelta()
   if (mixer) mixer.update(delta)
   requestAnimationFrame(render)
+  controls.update()
   renderer.render(scene, camera)
 }()
