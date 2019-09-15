3334daf2c6cba4ae5baf0270a7c74e59ae279ec3
Damjan Pavlica <mudroljub@gmail.com>
2019-09-15 18:38:09 +0200

    m

--- 50-procedural/52-procedural-city/game.js
+++ 50-procedural/52-procedural-city/game.js
@@ -5,31 +5,22 @@ import { createFloor } from '/utils/floor.js'
 import {randomInRange} from '/utils/helpers.js'
 import {FirstPersonControls} from '/node_modules/three/examples/jsm/controls/FirstPersonControls.js'
 
-camera.position.y = 80
+const size = 100
 
+camera.position.set(80, 80, 80)
 const controls = new FirstPersonControls(camera)
 controls.movementSpeed = 20
 controls.lookSpeed = 0.05
 
 scene.fog = new THREE.FogExp2(0xd0e0f0, 0.0025)
-scene.add(createFloor(2000, null, 0x101018))
+scene.add(createFloor(size * 2, null, 0x101018))
+
+const texture = new THREE.Texture(generateCityTexture())
+texture.needsUpdate = true
+
+for (let i = 0; i < size; i++) scene.add(generateBuilding())
 
 /* FUNCTIONS */
-
-function generateBuilding() {
-  const geometry = new THREE.CubeGeometry(1, 1, 1)
-  // change the pivot point to be at the bottom of the cube
-  geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0))
-  geometry.faces.splice(6, 2) // remove floor for optimization
-
-  const mesh = new THREE.Mesh(geometry)
-  mesh.position.x = randomInRange(-1000, 1000)
-  mesh.position.z = randomInRange(-1000, 1000)
-  mesh.rotation.y = Math.random()
-  mesh.scale.x = mesh.scale.z = randomInRange(10, 20)
-  mesh.scale.y = (Math.random() * Math.random() * Math.random() * mesh.scale.x) * 8 + 8
-  return mesh
-}
 
 function generateCityTexture() {
   const canvas = document.createElement('canvas')
@@ -37,32 +28,39 @@ function generateCityTexture() {
   canvas.height = 64
   const context = canvas.getContext('2d')
   context.fillStyle = '#ffffff'
-  context.fillRect(0, 0, 32, 64)
-  for (let y = 2; y < 64; y += 2)
-    for (let x = 0; x < 32; x += 2) {
-      const value = Math.floor(Math.random() * 64)
+  context.fillRect(0, 0, canvas.width, canvas.height)
+  for (let y = 2; y < canvas.height; y += 2)
+    for (let x = 0; x < canvas.width; x += 2) {
+      const value = Math.floor(Math.random() * canvas.height)
       context.fillStyle = `rgb(${value}, ${value}, ${value})`
       context.fillRect(x, y, 2, 1)
     }
   return canvas
 }
 
-function createCity(num = 10000) {
-  const cityGeo = new THREE.Geometry()
-  for (let i = 0; i < num; i++) {
-    const building = generateBuilding()
-    building.updateMatrix()
-    cityGeo.merge(building.geometry, building.matrix)
-  }
-  const texture = new THREE.Texture(generateCityTexture())
-  texture.needsUpdate = true
-  const material = new THREE.MeshLambertMaterial({map: texture})
-  return new THREE.Mesh(cityGeo, material)
+function generateBuilding() {
+  const geometry = new THREE.CubeGeometry(1, 1, 1)
+  geometry.faces.splice(6, 2) // remove floor for optimization
+  const num = randomInRange(100, 256, true)
+  const color = new THREE.Color(`rgb(${num}, ${num}, ${num})`) // random gray
+  const materials = [
+    new THREE.MeshLambertMaterial({ map: texture }),
+    new THREE.MeshLambertMaterial({ map: texture }),
+    new THREE.MeshLambertMaterial({ color }),
+    new THREE.MeshLambertMaterial({ map: texture }),
+    new THREE.MeshLambertMaterial({ map: texture }),
+    new THREE.MeshLambertMaterial({ map: texture }),
+  ]
+  const mesh = new THREE.Mesh(geometry, materials)
+  mesh.rotation.y = Math.random()
+  mesh.scale.x = mesh.scale.z = randomInRange(10, 20)
+  const scaleY = Math.random() * mesh.scale.x * 4 + 4
+  mesh.scale.y = scaleY
+  mesh.position.set(randomInRange(-size, size), scaleY / 2, randomInRange(-size, size))
+  return mesh
 }
 
 /* LOOP */
-
-scene.add(createCity(10000))
 
 void function animate() {
   requestAnimationFrame(animate)
--- 50-procedural/54-night-city/game.js
+++ 50-procedural/54-night-city/game.js
@@ -1,4 +1,3 @@
-// TODO: change roof color or texture
 import * as THREE from '/node_modules/three/build/three.module.js'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { createFloor } from '/utils/floor.js'
@@ -6,23 +5,19 @@ import {randomInRange} from '/utils/helpers.js'
 
 const size = 100
 
-camera.position.set(80, 80, 80)
 createOrbitControls()
 
 scene.fog = new THREE.FogExp2(0xd0e0f0, 0.0025)
 scene.add(createFloor(size * 2, null, 0x101018))
 
-const texture = new THREE.Texture(generateTexture())
+const texture = new THREE.Texture(generateCityTexture())
 texture.needsUpdate = true
 
-for (let i = 0; i < size; i++) {
-  const building = generateBuilding()
-  scene.add(building)
-}
+for (let i = 0; i < size; i++) scene.add(generateBuilding())
 
 /* FUNCTIONS */
 
-function generateTexture() {
+function generateCityTexture() {
   const canvas = document.createElement('canvas')
   canvas.width = 32
   canvas.height = 64
@@ -33,6 +28,7 @@ function generateTexture() {
     for (let x = 0; x < canvas.width; x += 2) {
       const value = Math.floor(Math.random() * canvas.height)
       context.fillStyle = `rgb(${value}, ${value}, ${value})`
+      if (Math.random() > .9) context.fillStyle = `rgb(${value * 2}, ${value * 2}, ${value})`
       context.fillRect(x, y, 2, 1)
     }
   return canvas
@@ -42,7 +38,7 @@ function generateBuilding() {
   const geometry = new THREE.CubeGeometry(1, 1, 1)
   geometry.faces.splice(6, 2) // remove floor for optimization
   const num = randomInRange(0, 30, true)
-  const color = new THREE.Color(`rgb(${num}, ${num}, ${num})`)
+  const color = new THREE.Color(`rgb(${num}, ${num}, ${num})`) // random gray
   const materials = [
     new THREE.MeshLambertMaterial({ map: texture }),
     new THREE.MeshLambertMaterial({ map: texture }),
