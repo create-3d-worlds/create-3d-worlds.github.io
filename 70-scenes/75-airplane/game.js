fd975f97691827394bd6f0490bc78c419fc1bfb3
Damjan Pavlica <damjan.pavlica@origo.is>
2021-11-26 10:10:52 +0100

    m

--- 70-scenes/75-airplane/game.js
+++ 70-scenes/75-airplane/game.js
@@ -1,6 +1,6 @@
 import * as THREE from '/node_modules/three108/build/three.module.js'
 import { ColladaLoader } from '/node_modules/three108/examples/jsm/loaders/ColladaLoader.js'
-import { createFullScene, renderer, createOrbitControls} from '/utils/scene.js'
+import { createFullScene, renderer, camera, createOrbitControls} from '/utils/scene.js'
 import { createTerrain } from '/utils/ground.js'
 import keyboard from '/classes/Keyboard.js'
 import Avion from './Avion.js'
@@ -10,9 +10,7 @@ let avion
 const scene = createFullScene({ color:0xFFC880 }, undefined, undefined, { color: 0xE5C5AB })
 scene.add(createTerrain(4000, 200))
 
-const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 5000)
-camera.position.set(0, 10, 250)
-const controls = createOrbitControls(camera)
+const controls = createOrbitControls()
 
 /* UPDATE */
 
@@ -23,7 +21,7 @@ const animate = () => {
   avion.normalizePlane()
   avion.position.z -= .5
   if (!keyboard.mouseDown)
-    camera.position.lerp({ ...avion.position, z: avion.position.z + 150 }, 0.05)
+    camera.position.lerp({ ...avion.position, z: avion.position.z + 100 }, 0.05)
   // camera.lookAt(avion.position)
   renderer.render(scene, camera)
 }
