b51131830ebeb0e26ee19533c4e6a5ed204e1335
Damjan Pavlica <damjan.pavlica@origo.is>
2021-11-28 04:16:14 +0100

    m

--- 70-scenes/70-airplane/game.js
+++ 70-scenes/70-airplane/game.js
@@ -1,3 +1,4 @@
+import * as THREE from '/node_modules/three108/build/three.module.js'
 import { createFullScene, renderer, camera, createOrbitControls } from '/utils/scene.js'
 import { createTerrain } from '/utils/ground.js'
 import { cameraFollowObject } from '/utils/helpers.js'
@@ -5,7 +6,11 @@ import keyboard from '/classes/Keyboard.js'
 import Airplane from '/classes/Airplane.js'
 
 const scene = createFullScene({ color:0xFFC880 }, undefined, undefined, { color: 0xE5C5AB })
-// scene.add(createTerrain(4000, 200))
+scene.add(createTerrain(4000, 200))
+
+const hemisphereLight = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75)
+hemisphereLight.position.set(0.5, 1, 0.75)
+scene.add(hemisphereLight)
 
 const controls = createOrbitControls()
 
