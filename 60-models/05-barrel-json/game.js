d98bf350acd67a70151f1e8f17ef1ae369a7cf5b
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-06 18:20:05 +0200

    rename

--- 60-models/05-barrel-json/game.js
+++ 60-models/05-barrel-json/game.js
@@ -4,22 +4,11 @@ import { scene, camera, renderer, createOrbitControls, initLights } from '/utils
 initLights()
 createOrbitControls()
 
-const textureLoader = new THREE.TextureLoader()
-
 camera.position.set(1, 1, 1)
 camera.lookAt(new THREE.Vector3(0, 0.4, 0))
 
-// commercial textures from http://gametextures.com/
-const map = textureLoader.load('textures/WoodBarrel_2k_d.jpg')
-const specularMap = textureLoader.load('textures/WoodBarrel_2k_s.jpg')
-const normalMap = textureLoader.load('textures/WoodBarrel_2k_n.jpg')
-const material = new THREE.MeshPhongMaterial({
-  map,
-  normalMap,
-  specularMap,
-  specular: 0xffffff,
-  shininess: 10,
-})
+const map = new THREE.TextureLoader().load('textures/WoodBarrel_2k_d.jpg')
+const material = new THREE.MeshPhongMaterial({ map })
 
 const loader = new THREE.LegacyJSONLoader()
 loader.load('/assets/models/barrel.js', geometry => {
