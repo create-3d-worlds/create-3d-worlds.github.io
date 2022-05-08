cf6881cf5e61db48f947da724213e957bfdca04b
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-08 13:01:11 +0200

    create loader

--- 60-models/60-konj-glb/game.js
+++ 60-models/60-konj-glb/game.js
@@ -1,21 +1,16 @@
 import * as THREE from '/node_modules/three108/build/three.module.js'
-import { GLTFLoader } from '/node_modules/three108/examples/jsm/loaders/GLTFLoader.js'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
 import { dirLight } from '/utils/light.js'
+import { loadGlb } from '/utils/loaders.js'
 
-let mixer
 let theta = 0
 
 dirLight({ color: 0xefefff, intensity: 1.5 })
 
-const loader = new GLTFLoader()
-loader.load('/assets/models/horse.glb', ({ scene: model, animations }) => {
-  model.scale.set(1.5, 1.5, 1.5)
-  scene.add(model)
-  console.log(animations)
-  mixer = new THREE.AnimationMixer(model)
-  mixer.clipAction(animations[0]).setDuration(1).play()
-})
+const { model, mixer } = await loadGlb({ glb: 'horse.glb', scale: 1.5 })
+scene.add(model)
+
+// FUNCTIONS
 
 function rotateCamera() {
   const radius = 600
@@ -24,6 +19,8 @@ function rotateCamera() {
   camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta))
   camera.lookAt(new THREE.Vector3(0, 200, 0))
 }
+
+// LOOP
 
 void function animate() {
   requestAnimationFrame(animate)
--- utils/loaders.js
+++ utils/loaders.js
@@ -1,19 +1,31 @@
 import * as THREE from '/node_modules/three108/build/three.module.js'
 import { OBJLoader } from '/node_modules/three108/examples/jsm/loaders/OBJLoader.js'
 import { MTLLoader } from '/node_modules/three108/examples/jsm/loaders/MTLLoader.js'
+import { GLTFLoader } from '/node_modules/three108/examples/jsm/loaders/GLTFLoader.js'
 
 export function loadObject({ mtl, obj, scale = 1 } = {}) {
+  const objLoader = new OBJLoader()
+  const mtlLoader = new MTLLoader()
+  mtlLoader.setMaterialOptions({ side: THREE.DoubleSide })
   return new Promise(resolve => {
-    const objLoader = new OBJLoader()
-    const mtlLoader = new MTLLoader()
-    mtlLoader.setMaterialOptions({ side: THREE.DoubleSide })
-
     mtlLoader.load(`/assets/models/${mtl}`, materials => {
       objLoader.setMaterials(materials)
       objLoader.load(`/assets/models/${obj}`, object => {
         object.scale.set(scale, scale, scale)
         resolve(object)
       })
+    })
+  })
+}
+
+export function loadGlb({ glb, scale = 1, autoplay = true } = {}) {
+  const loader = new GLTFLoader()
+  return new Promise(resolve => {
+    loader.load(`/assets/models/${glb}`, ({ scene: model, animations }) => {
+      model.scale.set(scale, scale, scale)
+      const mixer = new THREE.AnimationMixer(model)
+      if (autoplay) mixer.clipAction(animations[0]).setDuration(1).play()
+      resolve({ model, mixer, animations })
     })
   })
 }
