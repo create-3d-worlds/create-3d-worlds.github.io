284036ba8480a1c2e083ae87d9d03d447f9573bd
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-08 12:47:36 +0200

    pomalo

--- 60-models/60-konj-glb/game.js
+++ 60-models/60-konj-glb/game.js
@@ -1,42 +1,34 @@
 import * as THREE from '/node_modules/three108/build/three.module.js'
 import { GLTFLoader } from '/node_modules/three108/examples/jsm/loaders/GLTFLoader.js'
-import { scene, camera, renderer } from '/utils/scene.js'
+import { scene, camera, renderer, clock } from '/utils/scene.js'
+import { dirLight } from '/utils/light.js'
 
-let mesh, mixer
-const radius = 600
+let mixer
 let theta = 0
-let prevTime = Date.now()
 
-camera.target = new THREE.Vector3(0, 150, 0)
-
-const light = new THREE.DirectionalLight(0xefefff, 1.5)
-light.position.set(1, 1, 1).normalize()
-scene.add(light)
+dirLight({ color: 0xefefff, intensity: 1.5 })
 
 const loader = new GLTFLoader()
-loader.load('/assets/models/horse.glb', gltf => {
-  mesh = gltf.scene.children[0]
-  mesh.scale.set(1.5, 1.5, 1.5)
-  scene.add(mesh)
-  mixer = new THREE.AnimationMixer(mesh)
-  mixer.clipAction(gltf.animations[0]).setDuration(1).play()
+loader.load('/assets/models/horse.glb', ({ scene: model, animations }) => {
+  model.scale.set(1.5, 1.5, 1.5)
+  scene.add(model)
+  console.log(animations)
+  mixer = new THREE.AnimationMixer(model)
+  mixer.clipAction(animations[0]).setDuration(1).play()
 })
 
-function render() {
+function rotateCamera() {
+  const radius = 600
   theta += 0.1
   camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta))
   camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta))
-  camera.lookAt(camera.target)
-
-  if (mixer) {
-    const time = Date.now()
-    mixer.update((time - prevTime) * 0.001)
-    prevTime = time
-  }
-  renderer.render(scene, camera)
+  camera.lookAt(new THREE.Vector3(0, 200, 0))
 }
 
 void function animate() {
   requestAnimationFrame(animate)
-  render()
+  rotateCamera()
+  const delta = clock.getDelta()
+  if (mixer) mixer.update(delta)
+  renderer.render(scene, camera)
 }()
