57748a1588d60091c6aefefda9ae3799944bef78
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-09 10:03:14 +0200

    helpers

--- 60-models/20-castle-obj/main.js
+++ 60-models/20-castle-obj/main.js
@@ -1,14 +1,12 @@
-import { OBJLoader } from '/node_modules/three108/examples/jsm/loaders/OBJLoader.js'
 import { scene, camera, renderer, createOrbitControls, initLights } from '/utils/scene.js'
+import { loadObj } from '/utils/loaders.js'
 
 createOrbitControls()
 initLights()
 
-const loader = new OBJLoader()
-loader.load('/assets/models/zamak.obj', model => {
-  model.position.y = -95
-  scene.add(model)
-})
+const model = await loadObj({ obj: 'zamak.obj' })
+model.position.y = -95
+scene.add(model)
 
 /** LOOP **/
 
--- utils/loaders.js
+++ utils/loaders.js
@@ -3,20 +3,34 @@ import { OBJLoader } from '/node_modules/three108/examples/jsm/loaders/OBJLoader
 import { MTLLoader } from '/node_modules/three108/examples/jsm/loaders/MTLLoader.js'
 import { GLTFLoader } from '/node_modules/three108/examples/jsm/loaders/GLTFLoader.js'
 
-export function loadObj({ obj, mtl, scale = 1, rotateY = 0 } = {}) {
-  const objLoader = new OBJLoader()
-  const mtlLoader = new MTLLoader()
+const objLoader = new OBJLoader()
+const mtlLoader = new MTLLoader()
+const gtflLoader = new GLTFLoader()
+
+export const loadObj = params => params.mtl ? loadObjWithMtl(params) : loadObjOnly(params)
+
+export function loadObjOnly({ obj, scale = 1, rotateY = 0 } = {}) {
+  return new Promise(resolve => {
+    objLoader.load(`/assets/models/${obj}`, model => {
+      if (scale !== 1) model.scale.set(scale, scale, scale)
+      const mesh = new THREE.Group()
+      if (rotateY) model.rotateY(rotateY)
+      mesh.add(model)
+      resolve(mesh)
+    })
+  })
+}
+
+export function loadObjWithMtl({ obj, mtl, scale = 1, rotateY = 0 } = {}) {
   mtlLoader.setMaterialOptions({ side: THREE.DoubleSide })
   return new Promise(resolve => {
     mtlLoader.load(`/assets/models/${mtl}`, materials => {
       objLoader.setMaterials(materials)
       objLoader.load(`/assets/models/${obj}`, model => {
         if (scale !== 1) model.scale.set(scale, scale, scale)
-
         const mesh = new THREE.Group()
         if (rotateY) model.rotateY(rotateY)
         mesh.add(model)
-
         resolve(mesh)
       })
     })
@@ -24,17 +38,14 @@ export function loadObj({ obj, mtl, scale = 1, rotateY = 0 } = {}) {
 }
 
 export function loadGlb({ glb, scale = 1, rotateY = 0, autoplay = true } = {}) {
-  const loader = new GLTFLoader()
   return new Promise(resolve => {
-    loader.load(`/assets/models/${glb}`, ({ scene: model, animations }) => {
+    gtflLoader.load(`/assets/models/${glb}`, ({ scene: model, animations }) => {
       if (scale !== 1) model.scale.set(scale, scale, scale)
       const mixer = new THREE.AnimationMixer(model)
       if (autoplay) mixer.clipAction(animations[0]).play()
-
       const mesh = new THREE.Group()
       if (rotateY) model.rotateY(rotateY)
       mesh.add(model)
-
       resolve({ mesh, mixer, animations })
     })
   })
