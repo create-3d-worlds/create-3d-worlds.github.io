071382bfac46e1170a0507819b6cc84df80b19c8
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-10 21:23:24 +0200

    pomalo

--- 60-models/90-modeli-dae/game.js
+++ 60-models/90-modeli-dae/game.js
@@ -1,35 +1,33 @@
-import { ColladaLoader } from '/node_modules/three108/examples/jsm/loaders/ColladaLoader.js'
-import {scene, camera, renderer, createOrbitControls, initLights} from '/utils/scene.js'
+import { scene, camera, renderer, createOrbitControls, initLights } from '/utils/scene.js'
+import { loadModel } from '/utils/loaders.js'
 
 let currentId
 
 initLights()
-createOrbitControls()
-camera.position.set(-6, 2, 9)
+const controls = createOrbitControls()
 
-loadModel(document.querySelector('#izaberi-model').value)
+selectModel(document.querySelector('#select-model').value)
 
 /** FUNCTIONS **/
 
-function loadModel(file) {
-  const loader = new ColladaLoader()
-  loader.load(`/assets/models/${file}`, collada => {
-    scene.remove(scene.getObjectById(currentId))
-    const model = collada.scene
-    scene.add(model)
-    currentId = model.id
-  })
+async function selectModel(file) {
+  const { mesh } = await loadModel({ file })
+  scene.remove(scene.getObjectById(currentId))
+  scene.add(mesh)
+  controls.target = mesh.position
+  currentId = mesh.id
 }
 
 /** EVENTS **/
 
-document.querySelector('#izaberi-model').addEventListener('change', e => {
-  loadModel(e.target.value)
+document.querySelector('#select-model').addEventListener('change', e => {
+  selectModel(e.target.value)
 })
 
 /** LOOP **/
 
 void function update() {
   requestAnimationFrame(update)
+  controls.update()
   renderer.render(scene, camera)
 }()
