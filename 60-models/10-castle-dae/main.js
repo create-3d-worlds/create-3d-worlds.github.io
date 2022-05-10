db49af34cbbd9835c50dd06d669165886f672187
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-10 16:43:56 +0200

    m

--- 60-models/10-castle-dae/main.js
+++ 60-models/10-castle-dae/main.js
@@ -1,4 +1,3 @@
-import { ColladaLoader } from '/node_modules/three108/examples/jsm/loaders/ColladaLoader.js'
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { initLights } from '/utils/light.js'
 import { loadModel } from '/utils/loaders.js'
@@ -6,9 +5,9 @@ import { loadModel } from '/utils/loaders.js'
 initLights()
 
 const controls = createOrbitControls()
-camera.position.set(0, 2, 10)
+camera.position.set(0, 2, 15)
 
-const { mesh } = await loadModel({ file: 'tvrdjava.dae', size: 10, rot: { angle: Math.PI / 2, axis: [0, 1, 0] } })
+const { mesh } = await loadModel({ file: 'tvrdjava.dae', size: 10, rot: { angle: 2 * Math.PI, axis: [1, 0, 0] } })
 scene.add(mesh)
 
 /** LOOP **/
