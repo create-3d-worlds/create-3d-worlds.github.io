9c9838065738da223890c81a97004244d96aee5a
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-17 17:25:16 +0200

    m

--- 70-scenes/77-terrain-flamingo/game.js
+++ 70-scenes/77-terrain-flamingo/game.js
@@ -10,30 +10,30 @@ import Zeppelin from '/classes/aircrafts/Zeppelin.js'
 import keyboard from '/classes/Keyboard.js'
 import { loadModel } from '/utils/loaders.js'
 
+const controls = createOrbitControls()
+
 scene.add(createGradientSky({ r: 5000 }))
 const light = createSunLight({ x: 500, y: 2000, z: 100, far: 5000 })
 scene.add(light)
-scene.remove(scene.getObjectByName('hemisphereLight')) // BUG: sa svetlom puca terrain
+scene.remove(scene.getObjectByName('hemisphereLight'))
 scene.fog = new THREE.Fog(0xffffff, 1, 5000)
+
 const water = createGround({ color: 0x003133 })
 scene.add(water)
 
-const controls = createOrbitControls()
-
-const ground = createHillyTerrain({ size: 10000, y: 100, factorX: 5, factorZ: 2.5, factorY: 200 })
+const ground = createHillyTerrain()
 scene.add(ground)
 
 const { mesh, animations } = await loadModel({ file: 'ptice/flamingo.glb', size: 1, rot: { axis: [0, 1, 0], angle: Math.PI }, shouldCenter: true, shouldAdjustHeight: true })
 
-const flamingo = new Zeppelin({ mesh, animations, minHeight: 10, speed: .01 })
+const flamingo = new Zeppelin({ mesh, animations, minHeight: 10, speed: .05 })
 scene.add(mesh)
 
-mesh.position.y = 100
-camera.position.set(0, 103, 3)
+mesh.position.y = 30
 
 controls.target = mesh.position
-// scene.getObjectByName('sunLight').target = mesh
-// flamingo.addSolids(ground, water)
+scene.getObjectByName('sunLight').target = mesh
+flamingo.addSolids(ground, water)
 
 /* LOOP */
 
@@ -41,6 +41,6 @@ renderer.setAnimationLoop(() => {
   controls.update()
   flamingo.update()
 
-  if (!keyboard.pressed.mouse) cameraFollowObject(camera, flamingo.mesh, { distance: 1, y: 1 })
+  if (!keyboard.pressed.mouse) cameraFollowObject(camera, flamingo.mesh, { distance: 3, y: 2 })
   renderer.render(scene, camera)
 })
