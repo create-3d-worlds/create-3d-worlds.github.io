1c4eb6528427bc2519a6b2a51c92af3def72f7fa
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-19 19:39:28 +0200

    pomalo

--- 90-examples/30-endless-runner/game.js
+++ 90-examples/30-endless-runner/game.js
@@ -5,6 +5,7 @@ import { createSun } from './helpers/createSun.js'
 import { createFir } from '/utils/trees.js'
 import { createParticles } from './helpers/createParticles.js'
 import { hemLight } from '/utils/light.js'
+import { randomInRange } from '/utils/helpers.js'
 
 const rollingSpeed = 0.008
 const worldRadius = 26
@@ -13,14 +14,13 @@ const heroBaseY = 1.8
 const treeReleaseInterval = 0.5
 const treesInPool = 10
 const treesPool = []
-const treesInPath = []
+const laneTrees = []
 const lanes = [-1, 0, 1]
 const heroRollingSpeed = (rollingSpeed * worldRadius / heroRadius) / 5
-const pathAngleValues = [1.52, 1.57, 1.62]
 
+let explosionPower
 let laneIndex = 1
 let jumping = false
-let explosionPower = 1.06
 let bounceValue = 0.1
 
 /* LIGHT & CAMERA */
@@ -39,80 +39,87 @@ const player = createBall({ radius: heroRadius })
 player.position.set(lanes[laneIndex], heroBaseY, 4.8)
 scene.add(player)
 
-const world = createWorldSphere({ radius: worldRadius })
-world.position.set(0, -24, 2)
-scene.add(world)
+const earth = createWorldSphere({ radius: worldRadius })
+earth.position.set(0, -24, 2)
+scene.add(earth)
 
 const particles = createParticles(30)
 scene.add(particles)
 
 for (let i = 0; i < treesInPool; i++) treesPool.push(createFir({ size: 1 }))
 
-// side trees
 const numTrees = 36
 const gap = 6.28 / numTrees
 for (let i = 0; i < numTrees; i++) {
-  addTree(false, i * gap, true)
-  addTree(false, i * gap, false)
+  addSideTree(i * gap, true)
+  addSideTree(i * gap, false)
 }
 
 const updateScore = addScoreUI({ title: 'Pogotaka' })
 
 /* FUNCTIONS */
 
-function addTree(inPath, row, isLeft) {
+function addLaneTree(lane) {
+  if (treesPool.length == 0) return
+  const pathAngleValues = [1.52, 1.57, 1.62]
   const spherical = new THREE.Spherical()
-  let newTree
-  if (inPath) {
-    if (treesPool.length == 0) return
-    newTree = treesPool.pop()
-    newTree.visible = true
-    treesInPath.push(newTree)
-    spherical.set(worldRadius - 0.3, pathAngleValues[row], -world.rotation.x + 4)
-  } else {
-    newTree = createFir({ size: 5 })
-    const forestAreaAngle = isLeft ? 1.68 + Math.random() * 0.1 : 1.46 - Math.random() * 0.1
-    spherical.set(worldRadius - 0.3, forestAreaAngle, row)
-  }
-  newTree.position.setFromSpherical(spherical)
-  const worldVector = world.position.clone().normalize()
-  const treeVector = newTree.position.clone().normalize()
-  newTree.quaternion.setFromUnitVectors(treeVector, worldVector)
-  newTree.rotation.x += Math.random() * (2 * Math.PI / 10) - Math.PI / 10
-  world.add(newTree)
+  const tree = treesPool.pop()
+  tree.visible = true
+  laneTrees.push(tree)
+  spherical.set(worldRadius - 0.3, pathAngleValues[lane], -earth.rotation.x + 4)
+  addTree(tree, spherical)
 }
 
-function addPathTree() {
-  const options = [0, 1, 2]
+function addSideTree(theta, isLeft) {
+  const spherical = new THREE.Spherical()
+  const tree = createFir({ size: 5 })
+  const forestAreaAngle = isLeft ? 1.68 + Math.random() * 0.1 : 1.46 - Math.random() * 0.1
+  spherical.set(worldRadius - 0.3, forestAreaAngle, theta)
+  addTree(tree, spherical)
+}
+
+function addTree(tree, spherical) {
+  tree.position.setFromSpherical(spherical)
+  const worldVector = earth.position.clone().normalize()
+  const treeVector = tree.position.clone().normalize()
+  tree.quaternion.setFromUnitVectors(treeVector, worldVector)
+  tree.rotation.x += Math.random() * (2 * Math.PI / 10) - Math.PI / 10
+  earth.add(tree)
+}
+
+function addTreeOrTwo() {
+  const lanes = [0, 1, 2]
   const lane = Math.floor(Math.random() * 3)
-  addTree(true, lane)
-  options.splice(lane, 1)
+  addLaneTree(lane)
+  lanes.splice(lane, 1)
   if (Math.random() > 0.5) {
-    const secondLane = Math.floor(Math.random() * 2)
-    addTree(true, options[secondLane])
+    const anotherLane = Math.floor(Math.random() * 2)
+    addLaneTree(lanes[anotherLane])
   }
+}
+
+const hit = tree => {
+  explode()
+  updateScore()
+  tree.visible = false
+  setTimeout(() => {
+    tree.visible = true
+  }, 100)
 }
 
 function updateTrees() {
   const treePos = new THREE.Vector3()
-  const treesToRemove = []
-  treesInPath.forEach(tree => {
+  const distantTrees = []
+  laneTrees.forEach(tree => {
     if (!tree.visible) return
     treePos.setFromMatrixPosition(tree.matrixWorld)
     if (treePos.z > 6) // gone out of view
-      treesToRemove.push(tree)
-    else if (treePos.distanceTo(player.position) <= 0.6) {
-      explode()
-      updateScore()
-      tree.visible = false
-      setTimeout(() => {
-        tree.visible = true
-      }, 100)
-    }
+      distantTrees.push(tree)
+    else if (treePos.distanceTo(player.position) <= 0.6)
+      hit(tree)
   })
-  treesToRemove.forEach(tree => {
-    const fromWhere = treesInPath.indexOf(tree)
-    treesInPath.splice(fromWhere, 1)
+  distantTrees.forEach(tree => {
+    laneTrees.splice(laneTrees.indexOf(tree), 1)
     treesPool.push(tree)
     tree.visible = false
   })
@@ -129,16 +136,26 @@ function updateExplosion() {
 }
 
 function explode() {
-  particles.position.y = 2
-  particles.position.z = 4.8
-  particles.position.x = player.position.x
+  particles.position.set(player.position.x, 2, 4.8)
   particles.geometry.vertices.forEach(vertex => {
-    vertex.x = -0.2 + Math.random() * 0.4
-    vertex.y = -0.2 + Math.random() * 0.4
-    vertex.z = -0.2 + Math.random() * 0.4
+    vertex.x = randomInRange(-0.2, 0.2)
+    vertex.y = randomInRange(-0.2, 0.2)
+    vertex.z = randomInRange(-0.2, 0.2)
   })
   explosionPower = 1.07
   particles.visible = true
+}
+
+function updatePlayer() {
+  const gravity = 0.005
+  player.rotation.x -= heroRollingSpeed
+  if (player.position.y <= heroBaseY) {
+    jumping = false
+    bounceValue = (Math.random() * 0.04) + 0.005
+  }
+  player.position.y += bounceValue
+  player.position.x = THREE.Math.lerp(player.position.x, lanes[laneIndex], 2 * clock.getDelta())
+  bounceValue -= gravity
 }
 
 const jump = val => {
@@ -163,26 +180,14 @@ function movePlayer(e) {
     jump(0.1)
 }
 
-function updatePlayer() {
-  const gravity = 0.005
-  player.rotation.x -= heroRollingSpeed
-  if (player.position.y <= heroBaseY) {
-    jumping = false
-    bounceValue = (Math.random() * 0.04) + 0.005
-  }
-  player.position.y += bounceValue
-  player.position.x = THREE.Math.lerp(player.position.x, lanes[laneIndex], 2 * clock.getDelta())
-  bounceValue -= gravity
-}
-
 /* LOOP */
 
 void function update() {
-  world.rotation.x += rollingSpeed
+  earth.rotation.x += rollingSpeed
   updatePlayer()
   if (clock.getElapsedTime() > treeReleaseInterval) {
     clock.start()
-    addPathTree()
+    addTreeOrTwo()
   }
   updateTrees()
   updateExplosion()
