e4a44987bcbc0505e4160afebf19b5af49ab4c40
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-20 10:32:34 +0200

    m

--- 25-particles/10-stars/main.js
+++ 25-particles/10-stars/main.js
@@ -1,15 +1,14 @@
-import { scene, camera, renderer, createOrbitControls, hemLight } from '/utils/scene.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
 import { createParticles, resetParticles, moveParticles } from '/utils/particles.js'
 
 createOrbitControls()
-hemLight()
 
-const stars = createParticles({ file: 'star.png', num: 10000, color: 0xdddddd, size: .5, unitAngle: .2 })
+const stars = createParticles({ file: 'star.png', num: 10000, size: .5 })
 scene.add(stars)
 
-resetParticles({ particles: stars, unitAngle: .2 })
+resetParticles({ particles: stars })
 
-moveParticles({ particles: stars, min: 500, max: 1000 })
+moveParticles({ particles: stars, min: 200, max: 1000 })
 
 /* LOOP */
 
--- 25-particles/60-explosion/game.js
+++ 25-particles/60-explosion/game.js
@@ -2,14 +2,14 @@ import { scene, camera, renderer } from '/utils/scene.js'
 import { createParticles, resetParticles, moveParticles } from '/utils/particles.js'
 import { mouseToWorld } from '/utils/helpers.js'
 
-const particles = createParticles({ num: 30, color: 0xfffafa, size: 0.04, unitAngle: 0.1 })
+const particles = createParticles({ num: 30, color: 0xfffafa, size: 0.04 })
 scene.add(particles)
 
 /* LOOP */
 
 void function render() {
   renderer.render(scene, camera)
-  moveParticles({ particles, scalar: 1.1 }) // manje od jedan vraća unazad
+  moveParticles({ particles, scalar: 1.1 }) // scalar < 1 vraća unazad
   requestAnimationFrame(render)
 }()
 
