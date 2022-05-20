29636d707b82e29d7838c8012c98f9c935f36b80
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-20 17:17:00 +0200

    rename

--- 25-particles/10-stars/main.js
+++ 25-particles/10-stars/main.js
@@ -1,12 +1,12 @@
 import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
-import { createParticles, setPosition, moveParticles } from '/utils/particles.js'
+import { createParticles, setOrientation, moveParticles } from '/utils/particles.js'
 
 createOrbitControls()
 
 const stars = createParticles({ file: 'star.png', num: 10000, size: .5 })
 scene.add(stars)
 
-setPosition({ particles: stars })
+setOrientation({ particles: stars })
 
 moveParticles({ particles: stars, min: 200, max: 1000 })
 
--- 25-particles/15-flying-through-space/game.js
+++ 25-particles/15-flying-through-space/game.js
@@ -1,24 +1,18 @@
-import * as THREE from '/node_modules/three108/build/three.module.js'
 import { scene, camera, renderer } from '/utils/scene.js'
-import { randomInRange } from '/utils/helpers.js'
+import { createParticles, setOrientation, moveParticles } from '/utils/particles.js'
 
-const numberOfStars = 2000
-camera.position.z = numberOfStars
+camera.position.z = 500
 
-for (let i = 0; i < numberOfStars; i += 20) {
-  const geometry = new THREE.SphereGeometry(1)
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
-  const sphere = new THREE.Mesh(geometry, material)
-  sphere.position.x = randomInRange(-500, 500)
-  sphere.position.y = randomInRange(-500, 500)
-  sphere.position.z = i
-  scene.add(sphere)
-}
+const stars = createParticles({ file: 'star.png', num: 10000, size: .5 })
+scene.add(stars)
+
+setOrientation({ particles: stars })
+moveParticles({ particles: stars, min: 200, max: 1000 })
 
 /* LOOP */
 
 void function render() {
   requestAnimationFrame(render)
-  camera.position.z--
+  camera.position.z -= .3
   renderer.render(scene, camera)
 }()
