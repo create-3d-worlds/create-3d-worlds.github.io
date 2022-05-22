cfaa422e1e6aa628a19e6f963591c7bef13ba6d6
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-22 13:07:52 +0200

    pomalo

--- src/80-artificial-inteligence/50-leader-following/main.js
+++ src/80-artificial-inteligence/50-leader-following/main.js
@@ -1,63 +1,49 @@
 /* global THREE, SteeringEntity */
 import { camera, scene, renderer, createOrbitControls } from '/utils/scene.js'
 import { createFloor } from '/utils/ground.js'
+import { ambLight } from '/utils/light.js'
+import { createBox } from '/utils/boxes.js'
+import { randomInRange, getIntersects } from '/utils/helpers.js'
+
+ambLight()
 
 const controls = createOrbitControls()
-
-const light = new THREE.AmbientLight(0xffffff)
-scene.add(light)
-
-let entity1
-let boundaries
-let params
-
-let followers
-
 camera.position.set(0, 1000, 1000)
 
 const floor = createFloor({ size: 10000 })
 scene.add(floor)
 
-// Entity Mesh
-const geometry = new THREE.BoxGeometry(100, 200, 50)
-const material1 = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true })
-const mesh1 = new THREE.Mesh(geometry, material1)
-mesh1.position.setY(100)
+const mesh1 = createBox({ size: 100, yModifier: 2 })
 
-// Entities
-entity1 = new SteeringEntity(mesh1)
+const entity1 = new SteeringEntity(mesh1)
 entity1.maxSpeed = 15
 entity1.lookAtDirection = true
-entity1.position.set(Math.random() * (2500 - (-2500)) + (-2500), 0, Math.random() * (2500 - (-2500)) + (-2500))
+entity1.position.set(randomInRange(-2500, 2500), 0, randomInRange(-2500, 2500))
 entity1.wanderDistance = 10
 entity1.wanderRadius = 5
 entity1.wanderRange = 1
 scene.add(entity1)
 
-const material3 = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true })
+const params = { maxSpeed: 15, maxForce: 5, lookAtDirection: true, wanderDistance: 10, wanderRadius: 5, wanderRange: 1, numFollowers: 20, distance: 400, separationRadius: 300, maxSeparation: 500, leaderSightRadius: 1000, arrivalThreshold: 200 }
 
-params = { maxSpeed: 15, maxForce: 5, lookAtDirection: true, wanderDistance: 10, wanderRadius: 5, wanderRange: 1, numFollowers: 20, distance: 400, separationRadius: 300, maxSeparation: 500, leaderSightRadius: 1000, arrivalThreshold: 200 }
+const followers = []
 
-followers = []
-
-for (let i = 0;i < params.numFollowers;i++) {
-  const mesh = new THREE.Mesh(geometry, material3)
+for (let i = 0; i < params.numFollowers; i++) {
+  const mesh = createBox({ size: 100, yModifier: 2, color: 0x000000 })
   mesh.position.setY(100)
   const entity = new SteeringEntity(mesh)
-  entity.position.set(Math.random() * (2500 - (-2500)) + (-2500), 0, Math.random() * (2500 - (-2500)) + (-2500))
+  entity.position.set(randomInRange(-2500, 2500), 0, randomInRange(-2500, 2500))
   entity.maxSpeed = params.maxSpeed
   entity.maxForce = params.maxForce
   followers.push(entity)
   scene.add(entity)
 }
 
-// Plane boundaries (do not cross)
-boundaries = new THREE.Box3(new THREE.Vector3(-5000, 0, -5000), new THREE.Vector3(5000, 0, 5000))
+const boundaries = new THREE.Box3(new THREE.Vector3(-5000, 0, -5000), new THREE.Vector3(5000, 0, 5000))
 
-// Gui
+/* LOOP */
 
-animate()
-function animate() {
+void function animate() {
   requestAnimationFrame(animate)
   controls.update()
   entity1.wander()
@@ -67,7 +53,7 @@ function animate() {
   else
     entity1.rotation.set(0, 0, 0)
 
-  for (let i = 0;i < followers.length;i++) {
+  for (let i = 0; i < followers.length; i++) {
     followers[i].maxSpeed = params.maxSpeed
     followers[i].maxForce = params.maxForce
     followers[i].followLeader(entity1, followers, params.distance, params.separationRadius, params.maxSeparation, params.leaderSightRadius, params.arrivalThreshold)
@@ -83,17 +69,14 @@ function animate() {
   entity1.bounce(boundaries)
 
   renderer.render(scene, camera)
-}
+}()
+
+/* EVENTS */
 
 document.addEventListener('mousedown', onClick, true)
 
-function onClick(event) {
-  if (event) {
-    const mouse3D = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1, 0)
-    const raycaster = new THREE.Raycaster()
-    raycaster.setFromCamera(mouse3D, camera)
-    const intersects = raycaster.intersectObjects(scene.children)
-    if (intersects.length > 0)
-      entity1.position.set(intersects[0].point.x, intersects[0].point.y, intersects[0].point.z)
-  }
+function onClick(e) {
+  const intersects = getIntersects(e, camera, scene)
+  if (intersects.length > 0)
+    entity1.position.set(intersects[0].point.x, intersects[0].point.y, intersects[0].point.z)
 }
