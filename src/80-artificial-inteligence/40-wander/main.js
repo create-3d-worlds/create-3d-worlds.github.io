4944221db3ef8920829e313917a72bdc63c7450a
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-22 12:46:03 +0200

    eslint

--- src/80-artificial-inteligence/40-wander/main.js
+++ src/80-artificial-inteligence/40-wander/main.js
@@ -8,35 +8,32 @@ import { createBox } from '/utils/boxes.js'
 ambLight()
 
 const controls = createOrbitControls()
-camera.position.set(0, 1000, 1000)
+camera.position.set(0, 100, 100)
 
-const floor = createFloor({ size: 10000 })
+const floor = createFloor({ size: 1000 })
 scene.add(floor)
 
-const params = { maxSpeed: 10, maxForce: 5, lookAtDirection: true, wanderDistance: 10, wanderRadius: 5, wanderRange: 1, numEntities: 20 }
+const numEntities = 20
+const lookAtDirection = true
 
 const entities = []
-for (let i = 0;i < params.numEntities;i++) {
-  const mesh = createBox({ size: 100, yModifier: 2 })
+for (let i = 0; i < numEntities; i++) {
+  const mesh = createBox({ size: 10, yModifier: 2 })
   const entity = new SteeringEntity(mesh)
-  entity.position.set(randomInRange(-5000, 5000), 0, randomInRange(-5000, 5000))
+  entity.maxSpeed = 1
+  entity.position.set(randomInRange(-500, 500), 0, randomInRange(-500, 500))
   entities.push(entity)
   scene.add(entity)
 }
 
-const boundaries = new THREE.Box3(new THREE.Vector3(-5000, 0, -5000), new THREE.Vector3(5000, 0, 5000))
+const boundaries = new THREE.Box3(new THREE.Vector3(-500, 0, -500), new THREE.Vector3(500, 0, 500))
 
 void function animate() {
   requestAnimationFrame(animate)
 
-  for (let i = 0;i < entities.length;i++) {
-    entities[i].maxSpeed = params.maxSpeed
-    entities[i].maxForce = params.maxForce
-    entities[i].wanderDistance = params.wanderDistance
-    entities[i].wanderRadius = params.wanderRadius
-    entities[i].wanderRange = params.wanderRange
+  for (let i = 0; i < entities.length; i++) {
     entities[i].wander()
-    if (params.lookAtDirection)
+    if (lookAtDirection)
       entities[i].lookWhereGoing(true)
     else
       entities[i].rotation.set(0, 0, 0)
