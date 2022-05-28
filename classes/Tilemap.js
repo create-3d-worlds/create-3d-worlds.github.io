3f4c66aa3a66330dc699150d4963fb114284308d
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-28 18:03:44 +0200

    fix

--- classes/Tilemap.js
+++ classes/Tilemap.js
@@ -1,19 +1,15 @@
-import { randomMatrix, randomField, create3DMap } from '/utils/maps.js'
+import { randomMatrix, randomField, create3DMapOrigin } from '/utils/maps.js'
 
 export default class Tilemap {
   constructor(matrix = randomMatrix(), cellSize = 250, origin) {
+    const defaultOrigin = {
+      x: -matrix[0].length * cellSize / 2,
+      z: -matrix.length * cellSize / 2
+    }
     this.matrix = matrix
     this.cellSize = cellSize
     this.mapSize = (matrix.length - 1) * cellSize
-    this.origin = origin ? origin : { x: this.originX, z: this.originZ }
-  }
-
-  get originX() {
-    return -this.matrix[0].length * this.cellSize / 2
-  }
-
-  get originZ() {
-    return -this.matrix.length * this.cellSize / 2
+    this.origin = origin ? origin : defaultOrigin
   }
 
   getFieldValue(x, y) {
@@ -45,6 +41,6 @@ export default class Tilemap {
   }
 
   create3DMap({ yModifier } = {}) {
-    return create3DMap({ matrix: this.matrix, size: this.cellSize, yModifier, origin: this.origin })
+    return create3DMapOrigin({ matrix: this.matrix, size: this.cellSize, yModifier, origin: this.origin })
   }
 }
--- utils/maps.js
+++ utils/maps.js
@@ -11,22 +11,6 @@ export function randomMatrix(size = 10, wallPercent = .3) {
   return matrix
 }
 
-// export function create3DMap({ matrix = randomMatrix(), size = 1, yModifier } = {}) {
-//   const mapWidth = matrix.length
-//   const textures = ['concrete.jpg', 'crate.gif', 'brick.png']
-//   const group = new THREE.Group()
-//   matrix.forEach((row, rowIndex) => row.forEach((val, columnIndex) => {
-//     if (!val) return
-//     const x = (columnIndex - mapWidth / 2) * size
-//     const y = size / 2
-//     const z = (rowIndex - mapWidth / 2) * size
-//     const box = createBox({ size, file: textures[val - 1], yModifier })
-//     box.position.set(x, y, z)
-//     group.add(box)
-//   }))
-//   return group
-// }
-
 export function create3DMap({ matrix = randomMatrix(), size = 1, yModifier } = {}) {
   const mapWidth = matrix.length
   const group = new THREE.Group()
@@ -39,6 +23,19 @@ export function create3DMap({ matrix = randomMatrix(), size = 1, yModifier } = {
     const box = createBox({ size, file: textures[val - 1], yModifier })
     box.position.set(x, y, z)
     group.add(box)
+  }))
+  return group
+}
+
+// TODO: merge or deprecate
+export function create3DMapOrigin({ matrix = randomMatrix(), size = 1, yModifier, origin = { x: 0, z: 0 } } = {}) {
+  const textures = ['concrete.jpg', 'crate.gif', 'brick.png']
+  const group = new THREE.Group()
+  matrix.forEach((row, rowIndex) => row.forEach((val, columnIndex) => {
+    if (!val) return
+    const x = (columnIndex * size) + origin.x
+    const z = (rowIndex * size) + origin.z
+    group.add(createBox({ x, z, size, file: textures[val - 1], yModifier }))
   }))
   return group
 }
