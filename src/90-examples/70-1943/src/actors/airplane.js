356639d8a199ba773c930d883ebe15658c082fda
Damjan Pavlica <damjan.pavlica@origo.is>
2022-05-29 12:38:52 +0200

    sredjene komande

--- src/90-examples/70-1943/src/actors/airplane.js
+++ src/90-examples/70-1943/src/actors/airplane.js
@@ -9,11 +9,12 @@ const maxRoll = Math.PI / 3
 export function updatePlane(model, delta) {
   if (keyboard.left) {
     model.position.x += speed * delta
-    if (model.rotation.z < maxRoll) model.rotation.z += rotationSpeed * delta
+    if (model.rotation.z > -maxRoll) model.rotation.z -= rotationSpeed * delta
   }
   if (keyboard.right) {
     model.position.x -= speed * delta
-    if (model.rotation.z > -maxRoll) model.rotation.z -= rotationSpeed * delta
+    if (model.rotation.z < maxRoll) model.rotation.z += rotationSpeed * delta
+
   }
   if (keyboard.up)
     model.position.y += speed * 0.5 * delta
