0894e276025f2e4a1d45bc423cd6ced4f5ae4a8e
Damjan Pavlica <damjan.pavlica@origo.is>
2022-06-01 18:49:06 +0200

    pomalo

--- src/40-physics/45-move-object-physijs/main.js
+++ src/40-physics/45-move-object-physijs/main.js
@@ -1,58 +1,31 @@
 import * as THREE from '/node_modules/three119/build/three.module.js'
-import Physijs from '/libs/physi-ecma.js'
-import { camera, renderer, clock, createOrbitControls } from '/utils/scene.js'
+import { camera, renderer } from '/utils/scene.js'
 import { scene, createGround, createCrate } from '/utils/physics.js'
-
-const loader = new THREE.TextureLoader()
+import { randomInRange } from '/utils/helpers.js'
+import { CIRCLE } from '/utils/constants.js'
 
 let box
 let mouse_position
 const boxes = []
 
-camera.position.set(60, 50, 60)
+camera.position.set(30, 25, 30)
 camera.lookAt(scene.position)
 
-// Light
 const light = new THREE.DirectionalLight(0xFFFFFF, 0.6)
 light.position.set(20, 40, -15)
-light.target.position.copy(scene.position)
 light.castShadow = true
 scene.add(light)
 
 scene.add(new THREE.AmbientLight('white', 0.3))
 
-const box_material = Physijs.createMaterial(
-  new THREE.MeshLambertMaterial({ map: loader.load('/assets/textures/crate.gif') }),
-  .4, // low friction
-  .6 // high restitution
-)
-box_material.map.wrapS = THREE.RepeatWrapping
-box_material.map.repeat.set(.25, .25)
-
 const ground = createGround({ size: 100 })
 scene.add(ground)
 
 for (let i = 0; i < 10; i++) {
-  box = new Physijs.BoxMesh(
-    new THREE.BoxGeometry(4, 4, 4),
-    box_material
-  )
-  box.position.set(
-    Math.random() * 50 - 25,
-    10 + Math.random() * 5,
-    Math.random() * 50 - 25
-  )
-  box.rotation.set(
-    Math.random() * Math.PI * 2,
-    Math.random() * Math.PI * 2,
-    Math.random() * Math.PI * 2
-  )
-  box.scale.set(
-    Math.random() * 1 + .5,
-    Math.random() * 1 + .5,
-    Math.random() * 1 + .5
-  )
-  box.castShadow = true
+  box = createCrate({ size: 4, mass: 64 })
+  box.position.set(randomInRange(-25, 25), randomInRange(10, 15), randomInRange(-25, 25))
+  box.rotation.set(Math.random() * CIRCLE, Math.random() * CIRCLE, Math.random() * CIRCLE)
+  box.scale.set(randomInRange(.5, 1.5), randomInRange(.5, 1.5), randomInRange(.5, 1.5))
   scene.add(box)
   boxes.push(box)
 }
