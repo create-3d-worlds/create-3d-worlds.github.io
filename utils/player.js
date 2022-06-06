4fb4233e8fba35cd95fdd56009d951fd1a9c2167
Damjan Pavlica <damjan.pavlica@origo.is>
2022-06-06 22:43:50 +0200

    pomalo

--- utils/player.js
+++ utils/player.js
@@ -1,15 +1,13 @@
 import * as THREE from '/node_modules/three127/build/three.module.js'
 import keyboard from '/classes/Keyboard.js'
-import { camera } from '/utils/scene.js'
 import { RIGHT_ANGLE } from '/utils/constants.js'
 import { getHeight } from '/utils/helpers.js'
 
 let jumpCount = 0
 
-const jump = (mesh, delta) => {
-  const playerHeight = getHeight(mesh)
+const jump = (mesh, delta, playerHeight) => {
+  const maxJump = playerHeight
   const jumpSpeed = playerHeight * delta
-  const maxJump = playerHeight * 2
 
   jumpCount += jumpSpeed
   mesh.position.y = maxJump * Math.sin(jumpCount) + maxJump + playerHeight * .5 // sin could be -1 to 1
@@ -21,9 +19,9 @@ export function handleInput(mesh, delta) {
   const rotateDelta = RIGHT_ANGLE * delta // 90 degrees per second
 
   if (keyboard.pressed.Space)
-    jump(mesh, delta)
+    jump(mesh, delta, playerHeight)
 
-  if (!keyboard.pressed.Space && mesh.position.y > playerHeight * .5 + .01) jump(mesh, delta)
+  if (!keyboard.pressed.Space && mesh.position.y > playerHeight * .5 + .01) jump(mesh, delta, playerHeight)
 
   if (keyboard.up)
     mesh.translateZ(-speedDelta)
@@ -38,12 +36,6 @@ export function handleInput(mesh, delta) {
     mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotateDelta)
   if (keyboard.right)
     mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotateDelta)
-
-  if (keyboard.SwipeX)
-    camera.rotation.y -= keyboard.SwipeX * speedDelta * .0003
-
-  if (keyboard.SwipeY)
-    camera.rotation.x -= keyboard.SwipeY * speedDelta * .0003
 
   // for airplane
   // if (keyboard.pressed.KeyR)
