917820f3fa68e877f3e72f7293b4cbb3dc4c89db
Damjan Pavlica <damjan.pavlica@origo.is>
2022-06-08 21:48:15 +0200

    popravke, igrivo

---
+++ src/90-examples/42-nemesis-savo/main.js
@@ -0,0 +1,137 @@
+import { scene, camera, renderer, clock } from '/utils/scene.js'
+import {
+  getMapCell, createEnemy, isWall, createBullet, distance, isHit, randomXZ, moveBullet, remove, hitEnemy, moveEnemy
+} from './utils.js'
+import { UNITSIZE, NUM_AI, INITIAL_HEALTH, mapWidth } from './constants.js'
+import { normalizeMouse } from '/utils/helpers.js'
+import { dirLight } from '/utils/light.js'
+import { createFloor } from '/utils/ground.js'
+import { nemesis } from '/data/maps.js'
+import { create3DMap } from '/utils/maps.js'
+import FPSRenderer from '/classes/2d/FPSRenderer.js'
+import Savo from '/classes/Savo.js'
+
+const enemies = []
+const bullets = []
+let mouse = { x: 0, y: 0 }
+
+let runGame = false
+let kills = 0
+let health = INITIAL_HEALTH
+
+/* INIT */
+
+const fpsRenderer = new FPSRenderer()
+
+dirLight({ color: 0xF7EFBE, intensity: 0.7, position: [0.5, 1, 0.5] })
+dirLight({ color: 0xF7EFBE, intensity: 0.5, position: [-0.5, -1, -0.5] })
+
+const floor = createFloor({ size: mapWidth * UNITSIZE, file: 'ground.jpg' })
+const walls = create3DMap({ matrix: nemesis, size: UNITSIZE })
+scene.add(floor, walls)
+
+const player = new Savo({ size: 2.5 })
+player.add(camera)
+player.addSolids(walls)
+scene.add(player.mesh)
+
+/* FUNCTIONS */
+
+const removeEnemy = (el, i) => remove(enemies, el, i)
+
+const removeBullet = (el, i) => remove(bullets, el, i)
+
+const addBullet = (acter, target) => {
+  const mesh = createBullet(acter, target)
+  bullets.push(mesh)
+  scene.add(mesh)
+}
+
+function addEnemy() {
+  const mesh = createEnemy(randomXZ())
+  enemies.push(mesh)
+  scene.add(mesh)
+}
+
+function bulletHitEnemy(bullet, i) {
+  for (const enemy of enemies)
+    if (isHit(bullet, enemy)) {
+      removeBullet(bullet, i)
+      hitEnemy(enemy)
+      return true
+    }
+  return false
+}
+
+function checkBulletHitPlayer(bullet, i) {
+  if (distance(bullet.position, camera.position) > 25 || bullet.owner == camera) return
+  health = (health - 10 < 0) ? 0 : health - 10
+  document.querySelector('#health').innerHTML = health
+  removeBullet(bullet, i)
+}
+
+function updateBullets(delta) {
+  bullets.forEach((bullet, i) => {
+    if (isWall(bullet.position)) return removeBullet(bullet, i)
+    checkBulletHitPlayer(bullet, i)
+    if (!bulletHitEnemy(bullet, i)) moveBullet(bullet, delta)
+  })
+}
+
+const killEnemy = (enemy, i) => {
+  removeEnemy(enemy, i)
+  kills++
+  document.querySelector('#score').innerHTML = kills * 100
+}
+
+const checkEnemyFire = enemy => {
+  const enemyCell = getMapCell(enemy.position)
+  const playerCell = getMapCell(camera.position)
+  if (Date.now() - enemy.lastShot < 750 || distance(enemyCell, playerCell) > 1) return
+  addBullet(enemy)
+  enemy.lastShot = Date.now()
+}
+
+function updateEnemies(delta) {
+  enemies.forEach((enemy, i) => {
+    if (enemy.health <= 0) killEnemy(enemy, i)
+    moveEnemy(enemy, delta)
+    checkEnemyFire(enemy)
+  })
+}
+
+function reset() {
+  enemies.forEach(x => scene.remove(x))
+  bullets.forEach(x => scene.remove(x))
+  enemies.length = bullets.length = kills = 0
+  health = INITIAL_HEALTH
+  for (let i = 0; i < NUM_AI; i++) addEnemy()
+  runGame = true
+  gameLoop()
+}
+
+/* LOOP */
+
+function gameLoop() {
+  if (!runGame) return
+  requestAnimationFrame(gameLoop)
+  const delta = clock.getDelta()
+  player.update(delta)
+  updateBullets(delta)
+  updateEnemies(delta)
+  fpsRenderer.render(delta)
+  if (health <= 0)
+    runGame = false
+  renderer.render(scene, camera)
+}
+
+/* EVENTS */
+
+document.addEventListener('mousemove', e => {
+  mouse = normalizeMouse(e)
+})
+
+document.addEventListener('click', e => {
+  if (!runGame) reset()
+  if (e.button === 0) addBullet(camera, mouse)
+})
---
+++ src/90-examples/42-nemesis-savo/utils.js
@@ -0,0 +1,118 @@
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { camera, scene } from '/utils/scene.js'
+import { UNITSIZE, BULLETMOVESPEED, PROJECTILEDAMAGE, MOVESPEED, mapWidth, mapHeight } from './constants.js'
+import { randomInt } from '/utils/helpers.js'
+import { nemesis } from '/data/maps.js'
+import { getMapPosition } from '/utils/maps.js'
+
+const textureLoader = new THREE.TextureLoader()
+
+export function createHealth() {
+  const healthcube = new THREE.Mesh(
+    new THREE.BoxGeometry(30, 30, 30),
+    new THREE.MeshBasicMaterial({ map: textureLoader.load('images/health.png') })
+  )
+  healthcube.position.set(-UNITSIZE - 15, 35, -UNITSIZE - 15)
+  return healthcube
+}
+
+export const getMapCell = obj => getMapPosition({ obj, cellSize: UNITSIZE, map: nemesis })
+
+export function createEnemy({ x, z, size = UNITSIZE * .2 }) {
+  const geometry = new THREE.BoxGeometry(size, size, size)
+  const material = new THREE.MeshBasicMaterial({ map: textureLoader.load('images/face.png') })
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.position.set(x, UNITSIZE * 0.15, z)
+  mesh.health = 100
+  mesh.pathPos = 1
+  mesh.lastRandomX = Math.random()
+  mesh.lastRandomZ = Math.random()
+  mesh.lastShot = Date.now()
+  return mesh
+}
+
+export function isWall(v) {
+  const c = getMapCell(v)
+  return nemesis[c.z][c.x] > 0
+}
+
+export function createBullet(obj, target) {
+  const material = new THREE.MeshBasicMaterial({ color: 0x333333 })
+  const geometry = new THREE.SphereGeometry(UNITSIZE * .01, 6, 6)
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.position.set(obj.position.x, obj.position.y * 0.8, obj.position.z)
+  let vector
+  if (target) { // player is shooting
+    vector = new THREE.Vector3(target.x, target.y, 1)
+    vector.unproject(obj)
+  } else
+    vector = camera.position.clone()
+
+  mesh.ray = new THREE.Ray(obj.position, vector.sub(obj.position).normalize())
+  mesh.owner = obj
+  return mesh
+}
+
+export const distance = (a, b) => Math.sqrt((b.x - a.x) * (b.x - a.x) + (b.z - a.z) * (b.z - a.z))
+
+export const isHit = (bullet, target) => {
+  const bulletPos = bullet.position
+  const targetPos = target.position
+
+  const { position } = target.geometry.attributes
+  const vec = new THREE.Vector3()
+  vec.fromBufferAttribute(position, 0)
+  const x = Math.abs(vec.x)
+  const z = Math.abs(vec.z)
+  return bulletPos.x < targetPos.x + x && bulletPos.x > targetPos.x - x
+      && bulletPos.z < targetPos.z + z && bulletPos.z > targetPos.z - z
+      && bullet.owner != target
+}
+
+export const randomXZ = () => {
+  let x, z
+  const c = getMapCell(camera.position)
+  do {
+    x = randomInt(0, mapWidth - 1)
+    z = randomInt(0, mapHeight - 1)
+  } while (nemesis[z][x] > 0 || (x == c.x && z == c.z))
+
+  x = Math.floor(x - mapWidth / 2) * UNITSIZE
+  z = Math.floor(z - mapWidth / 2) * UNITSIZE
+  return { x, z }
+}
+
+export const moveBullet = (b, delta) => {
+  const speed = delta * BULLETMOVESPEED
+  b.translateX(speed * b.ray.direction.x)
+  b.translateZ(speed * b.ray.direction.z)
+}
+
+export const remove = (arr, el, i) => {
+  const index = i ? i : arr.findIndex(x => el.uuid == x.uuid)
+  arr.splice(index, 1)
+  scene.remove(el)
+}
+
+export const hitEnemy = enemy => {
+  enemy.health -= PROJECTILEDAMAGE
+  const { color } = enemy.material
+  const percent = enemy.health / 100
+  color.setRGB(percent * color.r, percent * color.g, percent * color.b)
+}
+
+export const moveEnemy = (enemy, delta) => {
+  const speed = delta * MOVESPEED
+  if (Math.random() > 0.995) {
+    enemy.lastRandomX = Math.random() * 2 - 1
+    enemy.lastRandomZ = Math.random() * 2 - 1
+  }
+  enemy.translateX(speed * enemy.lastRandomX)
+  enemy.translateZ(speed * enemy.lastRandomZ)
+  if (isWall(enemy.position)) {
+    enemy.translateX(-2 * speed * enemy.lastRandomX)
+    enemy.translateZ(-2 * speed * enemy.lastRandomZ)
+    enemy.lastRandomX = Math.random() * 2 - 1
+    enemy.lastRandomZ = Math.random() * 2 - 1
+  }
+}
