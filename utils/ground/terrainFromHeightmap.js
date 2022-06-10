acedb6c0c828ad2b2f0e5826821a232659fc54af
Damjan Pavlica <damjan.pavlica@origo.is>
2022-06-10 11:41:38 +0200

    m

--- utils/ground/terrainFromHeightmap.js
+++ utils/ground/terrainFromHeightmap.js
@@ -2,26 +2,15 @@ import * as THREE from '/node_modules/three127/build/three.module.js'
 
 const textureLoader = new THREE.TextureLoader()
 
-// TODO: možda izbaciti async
 function terrainFromHeightmap({
-  file = 'wiki.png', textureFile = '', widthSegments = 100, heightSegments = 100, displacementScale = 150, color = 0x33aa33,
+  file = 'wiki.png', textureFile = '', widthSegments = 100, heightSegments = 100, displacementScale = 150
 } = {}) {
-  const displacementMap = textureLoader.load(`/assets/heightmaps/${file}`)
-
   const geometry = new THREE.PlaneBufferGeometry(1000, 1000, widthSegments, heightSegments)
 
-  // const colors = []
-  // for (let i = 0, l = geometry.attributes.position.count; i < l; i ++) {
-  //   const nuance = similarColor(color)
-  //   colors.push(nuance.r, nuance.g, nuance.b)
-  // }
-  // geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
-
   const material = new THREE.MeshStandardMaterial({
-    color: 'gray',
-    // vertexColors: THREE.FaceColors,
+    color: 0x666666,
     map: textureFile ? textureLoader.load(`/assets/textures/${textureFile}`) : null,
-    displacementMap,
+    displacementMap: textureLoader.load(`/assets/heightmaps/${file}`),
     displacementScale,
   })
 
