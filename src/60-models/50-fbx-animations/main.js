3dc7120732731fbc7f6baa2d6adba9091351380c
Damjan Pavlica <damjan.pavlica@origo.is>
2022-06-11 11:51:54 +0200

    pomalo

---
+++ src/60-models/50-fbx-animations/main.js
@@ -0,0 +1,27 @@
+import * as THREE from '/node_modules/three127/build/three.module.js'
+import { FBXLoader } from '/node_modules/three127/examples/jsm/loaders/FBXLoader.js'
+import { scene, camera, renderer, createOrbitControls } from '/utils/scene.js'
+
+const light = new THREE.PointLight()
+light.position.set(0.8, 1.4, 1.0)
+scene.add(light)
+
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+
+const controls = createOrbitControls()
+controls.target.set(0, 1, 0)
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load('/assets/models/character-girl-fighter/girl-walk.fbx', object => {
+  object.scale.set(.02, .02, .02)
+  scene.add(object)
+})
+
+/* LOOP */
+
+void function animate() {
+  requestAnimationFrame(animate)
+  controls.update()
+  renderer.render(scene, camera)
+}()
