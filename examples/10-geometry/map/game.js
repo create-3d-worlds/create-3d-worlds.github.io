a0905116ca1c1205a8cf59160873f9f0085dff5c
Damjan Pavlica <mudroljub@gmail.com>
2023-03-25 15:07:40 +0100

    del data maps

--- examples/10-geometry/map/game.js
+++ examples/10-geometry/map/game.js
@@ -2,13 +2,25 @@ import { scene, renderer, camera, createOrbitControls } from '/utils/scene.js'
 import { meshFromTilemap } from '/utils/mazes.js'
 import { createGround } from '/utils/ground.js'
 import { hemLight } from '/utils/light.js'
-import { smallMap } from '/utils/data/maps.js'
+
+export const tilemap = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 2, 0, 1, 1, 1, 0, 1],
+  [1, 0, 2, 0, 0, 0, 0, 1, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+  [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 1, 0, 1, 1, 1, 0, 1],
+  [1, 1, 0, 1, 0, 0, 0, 1, 1, 1],
+  [1, 0, 0, 1, 0, 1, 0, 0, 0, 3],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
 hemLight()
 
 scene.add(createGround({ file: 'terrain/ground.jpg', size: 100 }))
 
-const map = meshFromTilemap({ tilemap: smallMap, texture: 'terrain/concrete.jpg' })
+const map = meshFromTilemap({ tilemap, texture: 'terrain/concrete.jpg' })
 scene.add(map)
 
 const controls = createOrbitControls()
