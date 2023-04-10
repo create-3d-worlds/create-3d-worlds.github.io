4a95e0471f1dd3bb7ca8a0f1f0f9dca91cc83662
Damjan Pavlica <mudroljub@gmail.com>
2023-04-10 13:47:13 +0200

    avatar-towers

--- examples/80-scenes/avatar-towers/game.js
+++ examples/80-scenes/avatar-towers/game.js
@@ -3,7 +3,6 @@ import { createSkySphere } from '/utils/geometry.js'
 import { createStairway, createBabelTower, createBaradDur, createRingTower } from '/utils/geometry/towers.js'
 import { createTerrain, createLava, createGround } from '/utils/ground.js'
 import { hemLight, dirLight } from '/utils/light.js'
-import { getShuffledCoords } from '/utils/helpers.js'
 import Avatar from '/utils/actor/Avatar.js'
 
 const mapSize = 1000
@@ -17,29 +16,26 @@ scene.add(terrain)
 
 /* BUILDING */
 
-const coords = getShuffledCoords({ mapSize: mapSize / 2, fieldSize: 50, emptyCenter: 100 })
-
-const stairsLeft = createStairway({ floors: 5 })
-stairsLeft.position.copy(coords.pop())
-stairsLeft.rotateY(Math.PI / 2)
-
-const stairsRight = createStairway({ floors: 5 })
-stairsRight.position.copy(coords.pop())
-stairsRight.rotateY(-Math.PI / 4)
-
 const babelTower = createBabelTower({ floors: 6 })
 const lava = createLava({ size: 50 })
 lava.translateY(4)
+
+const stairsLeft = createStairway({ floors: 5 })
+stairsLeft.position.set(80, 0, 0)
+stairsLeft.rotateY(Math.PI / 2)
+
+const stairsRight = createStairway({ floors: 5 })
+stairsRight.position.set(80, -20, 0)
 
 const lavaBottom = createGround({ size: 50 })
 lavaBottom.translateY(3.5)
 scene.add(lavaBottom)
 
 const baradDur = createBaradDur()
-baradDur.position.copy(coords.pop())
+baradDur.position.set(-150, 0, 150)
 
 const spaceTower = createRingTower()
-spaceTower.position.copy(coords.pop())
+spaceTower.position.set(-150, 0, -150)
 
 const solids = [terrain, stairsRight, stairsLeft, babelTower, baradDur, spaceTower, lavaBottom]
 scene.add(...solids, lava)
