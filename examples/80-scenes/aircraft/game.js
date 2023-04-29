36b36a1f9c7d5b1528c5997c720598f348fd6e7f
Damjan Pavlica <mudroljub@gmail.com>
2023-04-29 17:34:51 +0200

    m

--- examples/80-scenes/aircraft/game.js
+++ examples/80-scenes/aircraft/game.js
@@ -1,41 +1,26 @@
 import * as THREE from 'three'
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
-
 import { scene, renderer, camera, clock } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { loadModel } from '/utils/loaders.js'
 import { createGround, rotateGround } from '/utils/terrain/cylinder-ground.js'
 import { updatePlane, normalizePlane } from './utils/airplane.js'
 
-const createMixer = (mesh, animations, i = 0) => {
-  const mixer = new THREE.AnimationMixer(mesh)
-  const action = mixer.clipAction(animations[i])
-  action.play()
-  return mixer
-}
-
 scene.fog = new THREE.Fog(0xE5C5AB, 200, 950)
-
-scene.add(
-  new THREE.HemisphereLight(0xD7D2D2, 0x302B2F, .9),
-  createSun({ pos: [150, 350, -150] })
-)
+scene.add(new THREE.HemisphereLight(0xD7D2D2, 0x302B2F, .25))
+scene.add(createSun({ pos: [50, 250, 50] }))
 
 camera.position.set(-68, 143, -90)
-
 const controls = new OrbitControls(camera, renderer.domElement)
 
 const ground = createGround({ r: 3000, color: 0x91A566 })
 
-// const mesh = await loadModel({ file: '/aircraft_junkers_ju_87_stuka/scene.gltf', size: 30 })
-
 const mesh = await loadModel({ file: '/aircraft/messerschmitt-bf-109/scene.gltf', size: 20 })
 mesh.position.y = 100
-const mixer = createMixer(mesh, mesh.userData.animations)
 
 scene.add(mesh, ground)
 
-/* FUNCTIONS */
+/* LOOP */
 
 void function update() {
   requestAnimationFrame(update)
@@ -44,7 +29,6 @@ void function update() {
   rotateGround(ground)
   updatePlane(mesh, delta)
   normalizePlane(mesh, delta)
-  if (mixer) mixer.update(0.016)
   camera.lookAt(mesh.position)
   renderer.render(scene, camera)
 }()
--- examples/80-scenes/aircraft/utils/airplane.js
+++ examples/80-scenes/aircraft/utils/airplane.js
@@ -6,26 +6,26 @@ const rotationSpeed = .5
 const minHeight = 50
 const maxRoll = Math.PI / 3
 
-export function updatePlane(model, delta) {
+export function updatePlane(mesh, delta) {
   if (input.left) {
-    model.position.x += speed * delta
-    if (model.rotation.z > -maxRoll) model.rotation.z -= rotationSpeed * delta
+    mesh.position.x += speed * delta
+    if (mesh.rotation.z > -maxRoll) mesh.rotation.z -= rotationSpeed * delta
   }
   if (input.right) {
-    model.position.x -= speed * delta
-    if (model.rotation.z < maxRoll) model.rotation.z += rotationSpeed * delta
+    mesh.position.x -= speed * delta
+    if (mesh.rotation.z < maxRoll) mesh.rotation.z += rotationSpeed * delta
 
   }
   if (input.up)
-    model.position.y += speed * 0.5 * delta
+    mesh.position.y += speed * 0.5 * delta
 
   if (input.down)
-    if (model.position.y > minHeight) model.position.y -= speed * 0.5 * delta
+    if (mesh.position.y > minHeight) mesh.position.y -= speed * 0.5 * delta
 }
 
-export function normalizePlane(model, delta) {
+export function normalizePlane(mesh, delta) {
   if (input.keyPressed) return
-  const roll = Math.abs(model.rotation.z)
-  if (model.rotation.z > 0) model.rotation.z -= roll * delta * 2
-  if (model.rotation.z < 0) model.rotation.z += roll * delta * 2
+  const roll = Math.abs(mesh.rotation.z)
+  if (mesh.rotation.z > 0) mesh.rotation.z -= roll * delta * 2
+  if (mesh.rotation.z < 0) mesh.rotation.z += roll * delta * 2
 }
