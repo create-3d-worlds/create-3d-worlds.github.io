11ea56c2cfad4acb61b6e5878afad576fea8e792
Damjan Pavlica <mudroljub@gmail.com>
2023-04-29 21:49:32 +0200

    Aircraft

---
+++ examples/80-scenes/aircraft/Aircraft.js
@@ -0,0 +1,46 @@
+import input from '/utils/io/Input.js'
+
+export default class Aircraft {
+  constructor({ mesh }) {
+    this.mesh = mesh
+    mesh.position.y = 100
+    this.speed = 100
+    this.rotationSpeed = .5
+    this.minHeight = 50
+    this.maxRoll = Math.PI / 3
+  }
+
+  handleInput(delta) {
+    const { mesh } = this
+
+    if (input.left) {
+      mesh.position.x += this.speed * delta
+      if (mesh.rotation.z > -this.maxRoll) mesh.rotation.z -= this.rotationSpeed * delta
+    }
+
+    if (input.right) {
+      mesh.position.x -= this.speed * delta
+      if (mesh.rotation.z < this.maxRoll) mesh.rotation.z += this.rotationSpeed * delta
+    }
+
+    if (input.up)
+      mesh.position.y += this.speed * 0.5 * delta
+
+    if (input.down)
+      if (mesh.position.y > this.minHeight) mesh.position.y -= this.speed * 0.5 * delta
+  }
+
+  normalizePlane(delta) {
+    if (input.keyPressed) return
+    const { mesh } = this
+
+    const roll = Math.abs(mesh.rotation.z)
+    if (mesh.rotation.z > 0) mesh.rotation.z -= roll * delta * 2
+    if (mesh.rotation.z < 0) mesh.rotation.z += roll * delta * 2
+  }
+
+  update(delta) {
+    this.handleInput(delta)
+    this.normalizePlane(delta)
+  }
+}
--- examples/80-scenes/aircraft/game.js
+++ examples/80-scenes/aircraft/game.js
@@ -1,34 +1,32 @@
 import * as THREE from 'three'
-import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
 import { scene, renderer, camera, clock } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
 import { loadModel } from '/utils/loaders.js'
 import { createGround, rotateGround } from '/utils/terrain/cylinder-ground.js'
-import { updatePlane, normalizePlane } from './utils/airplane.js'
+import Aircraft from './Aircraft.js'
+
+camera.position.set(-68, 143, -90)
 
 scene.fog = new THREE.Fog(0xE5C5AB, 200, 950)
 scene.add(new THREE.HemisphereLight(0xD7D2D2, 0x302B2F, .25))
 scene.add(createSun({ pos: [50, 250, 50] }))
 
-camera.position.set(-68, 143, -90)
-const controls = new OrbitControls(camera, renderer.domElement)
-
 const ground = createGround({ r: 3000, color: 0x91A566 })
 
 const mesh = await loadModel({ file: '/aircraft/messerschmitt-bf-109/scene.gltf', size: 20 })
-mesh.position.y = 100
+const aircraft = new Aircraft({ mesh })
 
-scene.add(mesh, ground)
+scene.add(aircraft.mesh, ground)
 
 /* LOOP */
 
 void function update() {
   requestAnimationFrame(update)
   const delta = clock.getDelta()
-  controls.update()
+
   rotateGround(ground)
-  updatePlane(mesh, delta)
-  normalizePlane(mesh, delta)
+  aircraft.update(delta)
   camera.lookAt(mesh.position)
+
   renderer.render(scene, camera)
 }()
