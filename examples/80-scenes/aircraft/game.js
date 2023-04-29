a7bc5e7276c6884ae5c27ba9434c3e83cf0cf1c1
Damjan Pavlica <mudroljub@gmail.com>
2023-04-30 01:07:34 +0200

    refactor

--- examples/80-scenes/aircraft/game.js
+++ examples/80-scenes/aircraft/game.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { scene, renderer, camera, clock } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
-import CylinderGround from '/utils/terrain/CylinderGround.js'
+import { createGround } from '/utils/terrain/cylinder-ground.js'
 import Warplane from './Warplane.js'
 
 camera.position.set(-68, 143, -90)
@@ -10,10 +10,10 @@ scene.fog = new THREE.Fog(0xE5C5AB, 200, 950)
 scene.add(new THREE.HemisphereLight(0xD7D2D2, 0x302B2F, .25))
 scene.add(createSun({ pos: [50, 250, 50] }))
 
-const ground = new CylinderGround()
+const ground = new createGround()
 const aircraft = new Warplane()
 
-scene.add(aircraft.mesh, ground.mesh)
+scene.add(ground, aircraft.mesh)
 
 /* LOOP */
 
@@ -21,7 +21,7 @@ void function update() {
   requestAnimationFrame(update)
   const delta = clock.getDelta()
 
-  ground.update(delta)
+  ground.rotateX(-.25 * delta)
   aircraft.update(delta)
   camera.lookAt(aircraft.mesh.position)
 
---
+++ utils/terrain/cylinder-ground.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three'
+
+const deform = mesh => {
+  const vertex = new THREE.Vector3()
+  const { position } = mesh.geometry.attributes
+
+  for (let i = 0, l = position.count; i < l; i ++) {
+    vertex.fromBufferAttribute(position, i)
+    const angle = Math.random() * Math.PI * 2
+    const amp = 5 + Math.random() * 15
+    vertex.z += Math.cos(angle) * amp
+    vertex.y += Math.sin(angle) * amp
+    position.setXYZ(i, vertex.x, vertex.y, vertex.z)
+  }
+}
+
+export function createGround(r = 3000, height = 4000, color = 0x91A566) {
+  const geometry = new THREE.CylinderGeometry(r, r, height, 200, 50)
+  const matrix = new THREE.Matrix4().makeRotationX(Math.PI * .5).makeRotationZ(Math.PI * .5)
+  geometry.applyMatrix4(matrix)
+
+  const material = new THREE.MeshPhongMaterial({
+    color,
+    opacity: .8,
+    transparent: true,
+    flatShading: true,
+  })
+
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.receiveShadow = true
+  mesh.position.y = -r
+  deform(mesh)
+  return mesh
+}
