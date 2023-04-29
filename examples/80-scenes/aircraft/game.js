7066c806e9ad3c0c3ae0ef1ed88da20c463f5ad6
Damjan Pavlica <mudroljub@gmail.com>
2023-04-30 00:59:07 +0200

    CylinderGround

--- examples/80-scenes/aircraft/game.js
+++ examples/80-scenes/aircraft/game.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { scene, renderer, camera, clock } from '/utils/scene.js'
 import { createSun } from '/utils/light.js'
-import { createGround, rotateGround } from '/utils/terrain/cylinder-ground.js'
+import CylinderGround from '/utils/terrain/CylinderGround.js'
 import Warplane from './Warplane.js'
 
 camera.position.set(-68, 143, -90)
@@ -10,10 +10,10 @@ scene.fog = new THREE.Fog(0xE5C5AB, 200, 950)
 scene.add(new THREE.HemisphereLight(0xD7D2D2, 0x302B2F, .25))
 scene.add(createSun({ pos: [50, 250, 50] }))
 
-const ground = createGround({ r: 3000, color: 0x91A566 })
+const ground = new CylinderGround()
 const aircraft = new Warplane()
 
-scene.add(aircraft.mesh, ground)
+scene.add(aircraft.mesh, ground.mesh)
 
 /* LOOP */
 
@@ -21,7 +21,7 @@ void function update() {
   requestAnimationFrame(update)
   const delta = clock.getDelta()
 
-  rotateGround(ground)
+  ground.update(delta)
   aircraft.update(delta)
   camera.lookAt(aircraft.mesh.position)
 
