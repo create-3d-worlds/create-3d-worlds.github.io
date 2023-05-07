95c0960978481e5f3c5cbfbdc1edde537a265444
Damjan Pavlica <mudroljub@gmail.com>
2023-05-08 00:30:53 +0200

    m

--- utils/objects/Tower.js
+++ utils/objects/Tower.js
@@ -21,7 +21,12 @@ class Bullet extends GameObject {
   }
 }
 
-const mesh = await loadModel({ file: 'building/tower/ww2/D85VT1X9UHDSYASVUM1UY02HA.obj', mtl: 'building/tower/ww2/D85VT1X9UHDSYASVUM1UY02HA.mtl', size: 20, shouldAdjustHeight: true })
+const mesh = await loadModel({
+  file: 'building/tower/ww2/D85VT1X9UHDSYASVUM1UY02HA.obj',
+  mtl: 'building/tower/ww2/D85VT1X9UHDSYASVUM1UY02HA.mtl',
+  size: 20,
+  shouldAdjustHeight: true
+})
 
 export default class Tower extends GameObject {
   constructor(params = {}) {
