eb6752f00ce19b3d1061fcf269c762542376ada8
Damjan Pavlica <mudroljub@gmail.com>
2023-06-05 21:53:57 +0200

    refact

--- examples/80-scenes/partisans/main.js
+++ examples/80-scenes/partisans/main.js
@@ -2,6 +2,8 @@ const CANVAS = document.getElementsByTagName('canvas')[0]
 const SKY_BOX = document.getElementsByClassName('sky-box')[0]
 const CTX = CANVAS.getContext('2d')
 
+const randSpread = range => range * (Math.random() - Math.random())
+
 const SENSITIVITY = 0.04
 const SPRITES = []
 const CAMERA_INIT = {
@@ -19,7 +21,9 @@ const ELEMENTS = {
       'images/evergreen.png',
       'images/A_Lumpy_Evergreen.png',
       'images/Totally_Normal_Tree.png',
-    ]
+      'images/Boulder_Flintless.png',
+    ],
+    images: []
   },
   clouds: {
     number: 30,
@@ -27,26 +31,19 @@ const ELEMENTS = {
     origin: { x: 0, y: 2, z: 0 },
     urls: [
       'images/Gnat_Swarm.png'
-    ]
+    ],
+    images: []
   },
-  groundItems: {
-    number: 20,
-    range: { x: 10, y: 0, z: 10 },
-    origin: { x: 0, y: 0, z: 0 },
-    urls: [
-      'images/Boulder_Flintless.png',
-    ]
-  }
 }
 
-let width, height, camera
+let width, height
 let worldRot = 0
 let dWorldRot = 0
 let loadedImages = 0
 
-let imagesToLoad = 0
+let totalImages = 0
 for (const key in ELEMENTS)
-  imagesToLoad += ELEMENTS[key].urls.length
+  totalImages += ELEMENTS[key].urls.length
 
 /* CLASSES */
 
@@ -56,44 +53,57 @@ class Vector {
     this.y = y
     this.z = z
   }
-  project() {
-    const DZ = height / (camera.z - this.z)
-    const PX = (camera.x + this.x) * DZ
-    const PY = (camera.y + this.y) * DZ
-    return {
-      x: PX + width / 2,
-      y: PY + height / 2,
-      dz: DZ,
-      z: this.z
-    }
-  }
+
   rotate(ang) {
     const COS = Math.cos(ang)
     const SIN = Math.sin(ang)
     const X = this.x
-    const Y = this.y
     const Z = this.z
     this.x = X * COS - Z * SIN
     this.z = X * SIN + Z * COS
   }
 }
 
+const camera = new Vector(CAMERA_INIT.x, CAMERA_INIT.y, CAMERA_INIT.z)
+
 class Sprite {
-  constructor(v, images) {
+  constructor(el) {
+    const X = el.origin.x + randSpread(el.range.x)
+    const Y = el.origin.y + randSpread(el.range.y)
+    const Z = el.origin.z + randSpread(el.range.z)
+    this.v = new Vector(X, Y, Z)
     this.sz = 2
-    this.v = v
-    this.image = images[(Math.random() * images.length) | 0]
+    this.image = el.images[(Math.random() * el.images.length) | 0]
     this.xl = this.image.naturalWidth * this.sz
     this.yl = this.image.naturalHeight * this.sz
   }
+
   rotate(ang) {
     this.v.rotate(ang)
   }
+
+  outOfBounds(z, x, y, xl, yl) {
+    return z <= camera.z || x < 0 || y < 0 || x >= width - xl || y >= height - yl
+  }
+
+  project() {
+    const DZ = height / (camera.z - this.v.z)
+    const PX = (camera.x + this.v.x) * DZ
+    const PY = (camera.y + this.v.y) * DZ
+    return {
+      x: PX + width / 2,
+      y: PY + height / 2,
+      dz: DZ,
+      z: this.v.z
+    }
+  }
+
   render() {
-    const P = this.v.project()
+    const P = this.project()
     const SZ = P.dz / height
     const X_OFFS = this.xl / 6
-    if (outOfBounds(P.z, P.x + X_OFFS, P.y, this.xl * SZ, this.yl * SZ)) return
+    if (this.outOfBounds(P.z, P.x + X_OFFS, P.y, this.xl * SZ, this.yl * SZ)) return
+
     CTX.beginPath()
     CTX.drawImage(this.image, P.x + X_OFFS, P.y, this.xl * SZ, this.yl * SZ)
     CTX.closePath()
@@ -101,11 +111,6 @@ class Sprite {
 }
 
 /* FUNCTIONS */
-
-const outOfBounds = (z, x, y, xl, yl) =>
-  z <= camera.z || x < 0 || y < 0 || x >= width - xl || y >= height - yl
-
-const signedRandom = n => n * (Math.random() - Math.random())
 
 const renderSprites = () => {
   SPRITES.sort((a, b) => b.v.z - a.v.z)
@@ -127,19 +132,9 @@ const loop = () => {
   requestAnimationFrame(loop)
 }
 
-const create = e => {
-  for (let i = 0; i < e.number; ++i) {
-    const X = e.origin.x + signedRandom(e.range.x)
-    const Y = e.origin.y + signedRandom(e.range.y)
-    const Z = e.origin.z + signedRandom(e.range.z)
-    SPRITES.push(new Sprite(new Vector(X, Y, Z), e.images))
-  }
-}
-
-const go = () => {
-  camera = new Vector(CAMERA_INIT.x, CAMERA_INIT.y, CAMERA_INIT.z)
-  for (const key in ELEMENTS) create(ELEMENTS[key])
-  loop()
+const create = el => {
+  for (let i = 0; i < el.number; ++i)
+    SPRITES.push(new Sprite(el))
 }
 
 const setDim = () => {
@@ -150,16 +145,19 @@ const setDim = () => {
   CTX.scale(devicePixelRatio, devicePixelRatio)
 }
 
-const handleLoadImage = () => {
-  if (++loadedImages === imagesToLoad) go()
+const init = () => {
+  for (const key in ELEMENTS) create(ELEMENTS[key])
+  loop()
 }
 
-const preload = function(o) {
-  o.images = []
-  for (let i = 0; i < o.urls.length; ++i) {
-    o.images.push(new Image())
-    o.images[o.images.length - 1].src = o.urls[i]
-    o.images[o.images.length - 1].addEventListener('load', handleLoadImage)
+const preload = obj => {
+  for (let i = 0; i < obj.urls.length; ++i) {
+    const img = new Image()
+    img.src = obj.urls[i]
+    img.addEventListener('load', () => {
+      if (++loadedImages === totalImages) init()
+    })
+    obj.images.push(img)
   }
 }
 
