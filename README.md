b8d123667bc162210f1319524fbf2d22b8a49afd
Damjan Pavlica <mudroljub@gmail.com>
2023-06-15 19:26:02 +0200

    dodati nekim avionima dim ili različitu vatru

--- README.md
+++ README.md
@@ -16,8 +16,7 @@ npx live-server
 ### Scenes & examples
 
 - FPS / Airport
-  - dodati nekim avionima dim ili različitu vatru
-    - vremenom prestaje vatra
+  - vremenom prestaje vatra
   - dodati fabriku, ratne zgrade, tenk, bunker i slično
   - probati 3D model igrača
   - dodati pobedu / poraz
--- utils/objects/Building.js
+++ utils/objects/Building.js
@@ -1,14 +1,19 @@
+import * as THREE from 'three'
 import GameObject from '/utils/objects/GameObject.js'
+
+const { randFloat } = THREE.MathUtils
 
 export default class Building extends GameObject {
   constructor({ name = 'building', energy = 150, ...rest } = {}) {
     super({ name, energy, ...rest })
+    this.fireMin = randFloat(-8, -6)
+    this.fireMax = randFloat(2, 4)
   }
 
   addFire() {
     const promise = import('/utils/Particles.js')
     promise.then(obj => {
-      this.fire = new obj.Fire()
+      this.fire = Math.random() > .5 ? new obj.Fire() : new obj.BigSmoke()
       this.add(this.fire.mesh)
       this.fire.mesh.position.y += this.height * .5
       // this.fire.mesh.position.z -= this.depth * .5
@@ -23,6 +28,6 @@ export default class Building extends GameObject {
 
   update(delta) {
     super.update()
-    this.fire?.update({ delta })
+    this.fire?.update({ delta, min: this.fireMin, max: this.fireMax })
   }
 }
