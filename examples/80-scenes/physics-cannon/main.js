7fdfa8a73f9b4bddb88a50a6fca2b1c6cb51645a
Damjan Pavlica <mudroljub@gmail.com>
2023-06-27 11:00:09 +0200

    shoot

--- examples/80-scenes/physics-cannon/main.js
+++ examples/80-scenes/physics-cannon/main.js
@@ -1,20 +1,13 @@
-import * as THREE from 'three'
-import { Ammo } from '/utils/physics/index.js'
 import { scene, camera, renderer, clock } from '/utils/scene.js'
-import input from '/utils/io/Input.js'
 import { createSun } from '/utils/light.js'
-import { loadModel } from '/utils/loaders.js'
 import PhysicsWorld from '/utils/physics/PhysicsWorld.js'
 import { createGround } from '/utils/ground.js'
-import { createSphere, createSideWall } from '/utils/geometry/index.js'
+import { createSideWall } from '/utils/geometry/index.js'
 import Cannon from '/utils/physics/Cannon.js'
 
 let score
 
 const world = new PhysicsWorld()
-const impulse = document.getElementById('impulse')
-const minImpulse = impulse.value = 15
-const maxImpulse = 25
 
 const sun = createSun({ pos: [-5, 10, 5] })
 scene.add(sun)
@@ -26,43 +19,14 @@ const boxes = createSideWall({ brickMass: 3, friction: 5, z: 7 })
 boxes.forEach(mesh => world.add(mesh, -1))
 let countableCrates = boxes.filter(mesh => mesh.position.y > .5)
 
-const cannon = new Cannon({ physicsWorld: world.physicsWorld, camera})
+const cannon = new Cannon({ world, camera})
 scene.add(cannon.mesh, ...cannon.wheelMeshes)
-
-/* FUNCTIONS */
-
-function shoot() {
-  const angle = cannon.mesh.rotation.y // + Math.PI * .5
-  const x = impulse.value * Math.sin(angle)
-  const z = impulse.value * Math.cos(angle)
-
-  const distance = .7
-  const cannonTop = new THREE.Vector3(distance * Math.sin(angle), 0, distance * Math.cos(angle))
-
-  const pos = cannon.mesh.position.clone()
-  pos.y += 0.5
-  pos.add(cannonTop)
-
-  const ball = createSphere({ r: .2, color: 0x202020 })
-  ball.position.copy(pos)
-  world.add(ball, 4)
-
-  ball.userData.body.setLinearVelocity(new Ammo.btVector3(x, impulse.value * .2, z))
-  cannon.backward(impulse.value)
-  impulse.value = minImpulse
-}
-
-function handleInput() {
-  if (input.pressed.mouse && impulse.value < maxImpulse)
-    impulse.value = parseFloat(impulse.value) + .2
-}
 
 /* LOOP */
 
 void function loop() {
   requestAnimationFrame(loop)
   const dt = clock.getDelta()
-  handleInput()
   cannon.update(dt)
   world.update(dt)
 
@@ -75,10 +39,6 @@ void function loop() {
 
   renderer.render(scene, camera)
 }()
-
-/* EVENTS */
-
-document.addEventListener('mouseup', shoot)
 
 /* LAZY LOAD */
 
--- utils/physics/Cannon.js
+++ utils/physics/Cannon.js
@@ -1,12 +1,48 @@
+import * as THREE from 'three'
+import { Ammo } from '/utils/physics/index.js'
 import { loadModel } from '/utils/loaders.js'
 import Vehicle from '/utils/physics/Vehicle.js'
+import { createSphere } from '/utils/geometry/index.js'
 
 const mesh = await loadModel({ file: 'weapon/cannon/mortar/mortar.obj', mtl: 'weapon/cannon/mortar/mortar.mtl', size: 1, angle: Math.PI * .5 })
 
 export default class Cannon extends Vehicle {
-  constructor(param = {}) {
-    super({ mesh, defaultRadius: .18, wheelFront: { x: .3, y: .12, z: .32 }, wheelBack: { x: .3, y: .18, z: -.56 }, maxEngineForce: 20, mass: 100, ...param })
+  constructor({ world, ...rest } = {}) {
+    super({ mesh, physicsWorld: world.physicsWorld, defaultRadius: .18, wheelFront: { x: .3, y: .12, z: .32 }, wheelBack: { x: .3, y: .18, z: -.56 }, maxEngineForce: 20, mass: 100, ...rest })
+    this.world = world
     this.chaseCamera.offset = [0, 1, -2.5]
     this.chaseCamera.lookAt = [0, 1, 0]
+    this.impulse = document.getElementById('impulse')
+    this.minImpulse = this.impulse.value = 15
+    this.maxImpulse = 25
+
+    document.addEventListener('mouseup', this.shoot)
+  }
+
+  shoot = () => {
+    const angle = this.mesh.rotation.y // + Math.PI * .5
+    const x = this.impulse.value * Math.sin(angle)
+    const z = this.impulse.value * Math.cos(angle)
+
+    const distance = .7
+    const cannonTop = new THREE.Vector3(distance * Math.sin(angle), 0, distance * Math.cos(angle))
+
+    const pos = this.mesh.position.clone()
+    pos.y += 0.5
+    pos.add(cannonTop)
+
+    const ball = createSphere({ r: .2, color: 0x202020 })
+    ball.position.copy(pos)
+    this.world.add(ball, 4)
+
+    ball.userData.body.setLinearVelocity(new Ammo.btVector3(x, this.impulse.value * .2, z))
+    this.backward(this.impulse.value)
+    this.impulse.value = this.minImpulse
+  }
+
+  update(dt) {
+    super.update(dt)
+    if (this.input.pressed.mouse && this.impulse.value < this.maxImpulse)
+      this.impulse.value = parseFloat(this.impulse.value) + .2
   }
 }
