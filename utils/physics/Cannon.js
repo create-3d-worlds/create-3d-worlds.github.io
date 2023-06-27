e40a94979fa2eb4a8d4f17aacf2ccec9906272d5
Damjan Pavlica <mudroljub@gmail.com>
2023-06-27 11:08:43 +0200

    createInputRange

--- utils/physics/Cannon.js
+++ utils/physics/Cannon.js
@@ -6,13 +6,25 @@ import { createSphere } from '/utils/geometry/index.js'
 
 const mesh = await loadModel({ file: 'weapon/cannon/mortar/mortar.obj', mtl: 'weapon/cannon/mortar/mortar.mtl', size: 1, angle: Math.PI * .5 })
 
+function createInputRange() {
+  const impulse = document.createElement('input')
+  impulse.type = 'range'
+  impulse.min = 15
+  impulse.max = 25
+  impulse.step = 0.2
+  impulse.style.position = 'absolute'
+  document.body.prepend(impulse)
+  return impulse
+}
+
 export default class Cannon extends Vehicle {
   constructor({ world, ...rest } = {}) {
     super({ mesh, physicsWorld: world.physicsWorld, defaultRadius: .18, wheelFront: { x: .3, y: .12, z: .32 }, wheelBack: { x: .3, y: .18, z: -.56 }, maxEngineForce: 20, mass: 100, ...rest })
     this.world = world
     this.chaseCamera.offset = [0, 1, -2.5]
     this.chaseCamera.lookAt = [0, 1, 0]
-    this.impulse = document.getElementById('impulse')
+
+    this.impulse = createInputRange()
     this.minImpulse = this.impulse.value = 15
     this.maxImpulse = 25
 
